d7096e3f812022a87808998e12cf31ef8b6ba206
dintakurthianil <anilkumar.d@inhycgd6166.vertexcs.com>
2015-04-03 08:16:35 -0700

    Ramya-ODCServiceProfile
    Ramya-ODCServiceProfile

---
+++ New folder/ODCServiceProfiles.ascx
@@ -0,0 +1,46 @@
+﻿<%@ Assembly Name="$SharePoint.Project.AssemblyFullName$" %>
+<%@ Assembly Name="Microsoft.Web.CommandUI, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+<%@ Register TagPrefix="SharePoint" Namespace="Microsoft.SharePoint.WebControls" Assembly="Microsoft.SharePoint, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+<%@ Register TagPrefix="Utilities" Namespace="Microsoft.SharePoint.Utilities" Assembly="Microsoft.SharePoint, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+<%@ Register TagPrefix="asp" Namespace="System.Web.UI" Assembly="System.Web.Extensions, Version=4.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35" %>
+<%@ Import Namespace="Microsoft.SharePoint" %>
+<%@ Register TagPrefix="WebPartPages" Namespace="Microsoft.SharePoint.WebPartPages" Assembly="Microsoft.SharePoint, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="ODCServiceProfiles.ascx.cs" Inherits="vrtxIntranetSol.ODCServiceProfiles.ODCServiceProfiles" %>
+<SharePoint:CssRegistration runat="server" Name="/_layouts/15/VrtxIntranetStyles/CSS/intranet.css" />
+
+
+<div class="inner-left">
+    <div class="odcprofile-ctr" >
+        <h2>
+            <asp:Label runat="server" ID="lblTitle"></asp:Label>
+        </h2>
+        <div style="line-height:20px">
+        <p>
+            <asp:Label ID="lblDescription" runat="server"></asp:Label></p>
+            </div>
+        <h2>TEAM</h2>
+        <asp:Repeater ID="RepeaterImages" runat="server">
+
+            <ItemTemplate>
+                <div style="float: left; padding: 10px; border: solid; border-width: 0px; margin: 5px">
+                    <table>
+                        <tr>
+                            <td>
+                               <a href="/SitePages/ODCServiceProfiles.aspx?username=<%#Eval("Title") %>" Target="_blank">
+                                   <asp:Image ImageUrl='<%#Eval("ProfileTag")%>'   runat="server" Width="100" Height="100" /> </a>
+<%-- <asp:ImageButton ID="ImgUserPic" runat="server" Width="100" Height="100" ImageUrl='<%#Eval("ProfileTag")%>' />--%>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="font-weight: bold;text-align: center;">
+                                <asp:HyperLink ID="lblFirstName" Text='<%#Eval("Title") %>' runat="server" NavigateUrl='<%# Eval("Title", "/SitePages/ODCServiceProfiles.aspx?username={0}") %>' Target="_blank"></asp:HyperLink>
+                                <%-- <asp:HyperLink ID="lblUserName" Text='<%#Eval("LastName") %>' runat="server" ></asp:HyperLink>--%>
+                            </td>
+                        </tr>
+
+                    </table>
+                </div>
+            </ItemTemplate>
+        </asp:Repeater>
+    </div>
+</div>
---
+++ New folder/ODCServiceProfiles.ascx.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.SharePoint;
+using System;
+using System.ComponentModel;
+using System.Data;
+using System.Web;
+using System.Web.UI.WebControls.WebParts;
+
+namespace vrtxIntranetSol.ODCServiceProfiles
+{
+    [ToolboxItemAttribute(false)]
+    public partial class ODCServiceProfiles : WebPart
+    {
+        // Uncomment the following SecurityPermission attribute only when doing Performance Profiling on a farm solution
+        // using the Instrumentation method, and then remove the SecurityPermission attribute when the code is ready
+        // for production. Because the SecurityPermission attribute bypasses the security check for callers of
+        // your constructor, it's not recommended for production purposes.
+        // [System.Security.Permissions.SecurityPermission(System.Security.Permissions.SecurityAction.Assert, UnmanagedCode = true)]
+        public ODCServiceProfiles()
+        {
+        }
+
+        protected override void OnInit(EventArgs e)
+        {
+            base.OnInit(e);
+            InitializeControl();
+        }
+        string StrTech;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (HttpContext.Current.Request.QueryString["ProfileTags"] != null)
+            {
+                StrTech = HttpContext.Current.Request.QueryString["ProfileTags"];
+                GetProfileDescription(StrTech);
+                GetProfilePicture(StrTech);
+            }
+            else
+            {
+                StrTech = "Corporate Communication";
+                GetProfileDescription(StrTech);
+                GetProfilePicture(StrTech);
+            }
+        }
+        protected void GetProfilePicture(string ProfileTag)
+        {
+            StrTech = HttpContext.Current.Request.QueryString["ProfileTags"];
+            SPSecurity.RunWithElevatedPrivileges(delegate
+            {
+                using (SPSite site = new SPSite(SPContext.Current.Web.Url))
+                {
+                    using (SPWeb web = site.OpenWeb())
+                    {
+                        web.AllowUnsafeUpdates = true;
+                        SPList listBasicInfo = web.Lists.TryGetList("EmpBasicInfo");
+                        if (listBasicInfo != null)
+                        {
+
+                            SPQuery qry = new SPQuery();
+                            string stringQuery = "";
+                            stringQuery = string.Format(@"<Where><Eq><FieldRef Name='ProfileTag' /><Value Type='LookupMulti'>{0}</Value></Eq></Where>", StrTech);
+                            qry.Query = stringQuery;
+                            SPListItemCollection listcoll = listBasicInfo.GetItems(qry);
+                            DataTable dt = new DataTable();
+                            dt.Columns.Add("ProfileTag");
+                            dt.Columns.Add("Title");
+                            //dt.Columns.Add("FirstName");
+                            foreach (SPListItem item in listcoll)
+                            {
+                                DataRow dr = dt.NewRow();
+                                if (item.Attachments.Count > 0)
+                                {
+                                    foreach (String attachmentname in item.Attachments)
+                                    {
+                                        String attachmentAbsoluteURL = item.Attachments.UrlPrefix + attachmentname;
+                                        dr["ProfileTag"] = attachmentAbsoluteURL;                                        
+                                    }
+                                }
+                                else
+                                {
+                                    dr["ProfileTag"] = "/_layouts/15/VrtxIntranetStyles/Images/NoImage.jpg";
+                                }
+                                dr["Title"] = item["Title"];
+                                //dr["FirstName"] = item["FirstName"];
+                                dt.Rows.Add(dr);
+                            }
+                            RepeaterImages.DataSource = dt;
+                            RepeaterImages.DataBind();
+                        }
+                    }
+                }
+            });
+
+        }
+
+        protected void GetProfileDescription(string ProfileTag)
+        {
+            SPSecurity.RunWithElevatedPrivileges(delegate
+            {
+                using (SPSite site = new SPSite(SPContext.Current.Web.Url))
+                {
+                    using (SPWeb web = site.OpenWeb())
+                    {
+                        web.AllowUnsafeUpdates = true;
+                        SPList listProfileTag = web.Lists.TryGetList("ProfileTags");
+                        if (listProfileTag != null)
+                        {
+                            SPQuery qry = new SPQuery();
+                            string StrQuery = "";
+                            StrQuery = string.Format(@"<Where><Eq><FieldRef Name='Title' /><Value Type='Text'>{0}</Value></Eq></Where>", StrTech);
+                            qry.Query = StrQuery;
+                            SPListItemCollection listColl = listProfileTag.GetItems(qry);
+                            foreach (SPListItem item in listColl)
+                            {
+                                if(item["Description"]!=null)
+                                {
+                                    lblDescription.Text = item["Description"].ToString();
+                                }
+                                if (item["Title"]!= null)
+                                {
+                                    lblTitle.Text = item["Title"].ToString();
+                                }
+                            }
+                        }
+                    }
+                }
+            });
+
+        }
+    }
+}
