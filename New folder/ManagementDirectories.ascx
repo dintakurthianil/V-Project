6b1a7037845ac2e2fac2c760913968779005f94c
dintakurthianil <anilkumar.d@inhycgd6166.vertexcs.com>
2015-04-03 08:15:46 -0700

    ManagementDirectories
    ManagementDirectories

---
+++ New folder/ManagementDirectories.ascx
@@ -0,0 +1,54 @@
+﻿<%@ Assembly Name="$SharePoint.Project.AssemblyFullName$" %>
+<%@ Assembly Name="Microsoft.Web.CommandUI, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %> 
+<%@ Register Tagprefix="SharePoint" Namespace="Microsoft.SharePoint.WebControls" Assembly="Microsoft.SharePoint, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %> 
+<%@ Register Tagprefix="Utilities" Namespace="Microsoft.SharePoint.Utilities" Assembly="Microsoft.SharePoint, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+<%@ Register Tagprefix="asp" Namespace="System.Web.UI" Assembly="System.Web.Extensions, Version=4.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35" %>
+<%@ Import Namespace="Microsoft.SharePoint" %> 
+<%@ Register Tagprefix="WebPartPages" Namespace="Microsoft.SharePoint.WebPartPages" Assembly="Microsoft.SharePoint, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="ManagementDirectories.ascx.cs" Inherits="VrtxIntranetPortal.ManagementDirectories.ManagementDirectories" %>
+
+
+
+<SharePoint:CssRegistration runat="server" Name="/_layouts/15/VrtxIntranetStyles/CSS/intranet.css" />
+<asp:GridView ID="gvmangementDirectroies" runat="server" AutoGenerateColumns="false" Width="100%" BorderStyle="0" GridLines="None">
+    <AlternatingRowStyle CssClass="grid-sub2" BorderWidth="0" />
+    <RowStyle CssClass="grid-sub1" BorderWidth="0" />
+    <HeaderStyle CssClass="grid-header" />
+
+    <Columns>
+         <asp:TemplateField HeaderText="S.No" HeaderStyle-HorizontalAlign="Left"  > 
+            <ItemTemplate>
+               <asp:Label ID="lblSerialNo" Text='<%#Container.DataItemIndex + 1%>'  runat="server"></asp:Label>
+            </ItemTemplate>
+        </asp:TemplateField>
+        <asp:TemplateField HeaderText="Name" HeaderStyle-HorizontalAlign="Left">
+            <ItemTemplate>
+               <asp:Label ID="lblTitle" Text='<%#Eval("Title")%>'  runat="server"></asp:Label>
+            </ItemTemplate>
+        </asp:TemplateField>
+         <asp:TemplateField HeaderText="Designation" HeaderStyle-HorizontalAlign="Left">
+            <ItemTemplate>
+               <asp:Label ID="lblDesignation" Text='<%#Eval("Designation")%>'  runat="server"></asp:Label>
+            </ItemTemplate>
+        </asp:TemplateField>
+         <asp:TemplateField HeaderText="Phone Number" HeaderStyle-HorizontalAlign="Left">
+            <ItemTemplate>
+               <asp:Label ID="lblPhoneNumber" Text='<%#Eval("PhoneNumber")%>'  runat="server"></asp:Label>
+            </ItemTemplate>
+        </asp:TemplateField>
+         <asp:TemplateField HeaderText="ICQ No" HeaderStyle-HorizontalAlign="Left">
+            <ItemTemplate>
+               <asp:Label ID="lblICQNo" Text='<%#Eval("ICQNo")%>'  runat="server"></asp:Label>
+            </ItemTemplate>
+        </asp:TemplateField>
+        <asp:TemplateField HeaderText="Vertex Mail ID" HeaderStyle-HorizontalAlign="Left">
+            <ItemTemplate>
+              <asp:Label ID="lblEmail" Text='<%#Eval("VertexMailID")%>'  runat="server"></asp:Label>
+            </ItemTemplate>
+        </asp:TemplateField>
+    </Columns>
+</asp:GridView>
+
+<div style="padding-left: 30%;padding-top: 10%;">
+    <h2><asp:Label Text="" ID="lblError" Visible="false" runat="server" /></h2>
+</div>
---
+++ New folder/ManagementDirectories.ascx.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.SharePoint;
+using System;
+using System.ComponentModel;
+using System.Data;
+using System.Web.UI.WebControls.WebParts;
+using System.Text;
+
+namespace VrtxIntranetPortal.ManagementDirectories
+{
+    [ToolboxItemAttribute(false)]
+    public partial class ManagementDirectories : WebPart
+    {
+        // Uncomment the following SecurityPermission attribute only when doing Performance Profiling on a farm solution
+        // using the Instrumentation method, and then remove the SecurityPermission attribute when the code is ready
+        // for production. Because the SecurityPermission attribute bypasses the security check for callers of
+        // your constructor, it's not recommended for production purposes.
+        // [System.Security.Permissions.SecurityPermission(System.Security.Permissions.SecurityAction.Assert, UnmanagedCode = true)]
+        public ManagementDirectories()
+        {
+        }
+
+        protected override void OnInit(EventArgs e)
+        {
+            base.OnInit(e);
+            InitializeControl();
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            GetMangeDirectories();
+        }
+
+        private void GetMangeDirectories()
+        {
+            using (SPSite osite = new SPSite(SPContext.Current.Web.Url))
+            {
+                using (SPWeb oweb = osite.OpenWeb())
+                {
+                    SPQuery qry = new SPQuery();
+                    SPList olist = oweb.Lists.TryGetList("Management Directories");                    
+                    SPListItemCollection collListItems = olist.GetItems();
+                    DataTable dt = new DataTable();
+                    dt.Columns.Add("Title", typeof(string));
+                    dt.Columns.Add("Designation", typeof(string));
+                    dt.Columns.Add("PhoneNumber", typeof(string));
+                    dt.Columns.Add("ICQNo", typeof(string));
+                    dt.Columns.Add("VertexMailID", typeof(string));
+                    if (collListItems.Count > 0)
+                    {
+                        foreach (SPListItem item in collListItems)
+                        {
+                            DataRow dr = dt.NewRow();
+                            dr["Title"] = item["Title"] != null ? item["Title"].ToString() : string.Empty;
+                            dr["Designation"] = item["Designation"] != null ? item["Designation"].ToString() : string.Empty;
+                            dr["PhoneNumber"] = item["PhoneNumber"] != null ? item["PhoneNumber"].ToString() : string.Empty;
+                            dr["ICQNo"] = item["ICQNo"] != null ? item["ICQNo"].ToString() : string.Empty;
+                            StringBuilder st=new StringBuilder();
+                            st.AppendFormat("<a href=mailto:"+item["VertexMailID"].ToString()+" style=cursor:hand>"+item["VertexMailID"].ToString()+"</a>");
+                            dr["VertexMailID"] = item["VertexMailID"] != null ? st.ToString() : string.Empty;
+                            dt.Rows.Add(dr);
+                        }
+                        gvmangementDirectroies.DataSource = dt;
+                        gvmangementDirectroies.DataBind();
+                    }
+                    else
+                    {
+                        lblError.Visible = true;
+                        lblError.Text = "No Data Found";
+                    }
+                }
+            }
+        }
+    }
+}
