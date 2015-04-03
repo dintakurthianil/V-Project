90b99b996126d8afc12f70c87f15b9fc44652db8
dintakurthianil <anilkumar.d@inhycgd6166.vertexcs.com>
2015-04-03 08:11:40 -0700

    Holiydays
    Holiydays

---
+++ New folder/Holidays.ascx
@@ -0,0 +1,89 @@
+﻿<%@ Assembly Name="$SharePoint.Project.AssemblyFullName$" %>
+<%@ Assembly Name="Microsoft.Web.CommandUI, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %> 
+<%@ Register Tagprefix="SharePoint" Namespace="Microsoft.SharePoint.WebControls" Assembly="Microsoft.SharePoint, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %> 
+<%@ Register Tagprefix="Utilities" Namespace="Microsoft.SharePoint.Utilities" Assembly="Microsoft.SharePoint, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+<%@ Register Tagprefix="asp" Namespace="System.Web.UI" Assembly="System.Web.Extensions, Version=4.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35" %>
+<%@ Import Namespace="Microsoft.SharePoint" %> 
+<%@ Register Tagprefix="WebPartPages" Namespace="Microsoft.SharePoint.WebPartPages" Assembly="Microsoft.SharePoint, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="Holidays.ascx.cs" Inherits="VrtxIntranetPortal.Holidays.Holidays" %>
+
+<script>
+    function printDiv(printcontent) {
+        var contents = document.getElementById(printcontent).innerHTML;
+        var frame1 = document.createElement('iframe');
+        frame1.name = "frame1";
+        frame1.style.position = "absolute";
+        frame1.style.top = "-1000000px";
+        document.body.appendChild(frame1);
+        var frameDoc = frame1.contentWindow ? frame1.contentWindow : frame1.contentDocument.document ? frame1.contentDocument.document : frame1.contentDocument;
+        frameDoc.document.open();
+        frameDoc.document.write('<html><head><title></title>');
+        frameDoc.document.write('</head><body>');
+        frameDoc.document.write(contents);
+        frameDoc.document.write('</body></html>');
+        frameDoc.document.close();
+        setTimeout(function () {
+            window.frames["frame1"].focus();
+            window.frames["frame1"].print();
+            document.body.removeChild(frame1);
+        }, 500);
+        return false;
+        //var printContents = document.getElementById(printcontent).innerHTML;
+        //var originalContents = document.body.innerHTML;
+        //document.body.innerHTML = printContents;
+        //window.print();
+        //document.body.innerHTML = originalContents;
+    }
+</script>
+
+<style>
+    
+
+</style>
+
+<SharePoint:CssRegistration runat="server" Name="/_layouts/15/VrtxIntranetStyles/CSS/intranet.css" />
+<a style="padding-left:98%"  onclick="printDiv('printcontent')" ><asp:Image  ImageUrl="/_layouts/15/VrtxIntranetStyles/Images/printer-icon.png" runat="server" /></a>
+<%--<input type="button" id="btnPrint" value="Print" onclick="printDiv('printcontent')" />--%>
+<%--<asp:Label ID="akjsdfh" runat="server" ></asp:Label>--%>
+<div id="printcontent">
+<asp:GridView ID="gvHolidays" runat="server" AutoGenerateColumns="false" Width ="100%" BorderStyle="0"   GridLines="None">
+    <AlternatingRowStyle CssClass="grid-sub2" BorderWidth="0" />
+    <RowStyle CssClass="grid-sub1" BorderWidth="0"  />
+    <HeaderStyle CssClass="grid-header" />
+
+    <Columns>
+        <asp:TemplateField HeaderText="S.No" HeaderStyle-HorizontalAlign="Left"  > 
+            <ItemTemplate>
+               <asp:Label ID="lblSerialNo" Text='<%#Container.DataItemIndex + 1%>' Style="padding: 10px;"  runat="server"></asp:Label>
+            </ItemTemplate>
+        </asp:TemplateField>
+        <asp:TemplateField HeaderText="Month" HeaderStyle-HorizontalAlign="Left">
+            <ItemTemplate>
+               <asp:Label ID="lblMonth" Text='<%#Eval("Month")%>'  runat="server"></asp:Label>
+            </ItemTemplate>
+        </asp:TemplateField>
+        <asp:TemplateField HeaderText="Day" HeaderStyle-HorizontalAlign="Left">
+            <ItemTemplate>
+               <asp:Label ID="lblDay" Text='<%#Eval("Day")%>'  runat="server"></asp:Label>
+            </ItemTemplate>
+        </asp:TemplateField>
+         <asp:TemplateField HeaderText="Date" HeaderStyle-HorizontalAlign="Left">
+            <ItemTemplate>
+               <asp:Label ID="lblDateAndDay" Text='<%#Eval("HolidayDate")%>' runat="server"></asp:Label>
+            </ItemTemplate>
+        </asp:TemplateField>
+        <asp:TemplateField HeaderText="Holidays" HeaderStyle-HorizontalAlign="Left">
+            <ItemTemplate>
+               <asp:Label ID="lblHolidays" Text='<%#Eval("HolidayName")%>' runat="server"></asp:Label>
+            </ItemTemplate>
+        </asp:TemplateField>
+    </Columns>
+</asp:GridView>
+
+    </div>
+<br />
+<asp:Panel  ID="pnlComment" runat="server" ></asp:Panel>
+<div ID="lblPGResult" runat="server"></div>
+
+
+
---
+++ New folder/Holidays.ascx.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.ComponentModel;
+using System.Web.UI.WebControls.WebParts;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.Client;
+using Microsoft.SharePoint.Client.ServerRuntime;
+using Microsoft.SharePoint.Administration;
+using System.Text;
+using Microsoft.SharePoint.Utilities;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web;
+
+
+namespace VrtxIntranetPortal.Holidays
+{
+    [ToolboxItemAttribute(false)]
+    public partial class Holidays : WebPart
+    {
+        // Uncomment the following SecurityPermission attribute only when doing Performance Profiling on a farm solution
+        // using the Instrumentation method, and then remove the SecurityPermission attribute when the code is ready
+        // for production. Because the SecurityPermission attribute bypasses the security check for callers of
+        // your constructor, it's not recommended for production purposes.
+        // [System.Security.Permissions.SecurityPermission(System.Security.Permissions.SecurityAction.Assert, UnmanagedCode = true)]
+        public Holidays()
+        {
+        }
+        int serialNo = 0;
+        protected override void OnInit(EventArgs e)
+        {
+            base.OnInit(e);
+            InitializeControl();
+        }
+        private int count = 0;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                string QurCountry = string.Empty;
+                try
+                {
+                    if (HttpContext.Current.Request.QueryString["Ctype"] != null && HttpContext.Current.Request.QueryString["Ctype"] != string.Empty)
+                    {
+                        QurCountry = HttpContext.Current.Request.QueryString["Ctype"];
+                        if (QurCountry != "pg")
+                        {
+                            GetHolidays(QurCountry, true);
+                        }
+                        else if (QurCountry == "pg")
+                        {
+                            GetHolidays(QurCountry, false);
+                        }
+                    }
+                    else
+                    {
+                        StringBuilder sb = new StringBuilder();
+
+                        sb.Append("<table style=Width:400px><tr Class=grid-sub1><td Class=grid-header>S.No</td><td  Class=grid-header>Month</td><td  Class=grid-header>Day</td><td Class=grid-header>Date</td><td Class=grid-header>Holidays</td></tr><tr  Class=grid-sub1><td colspan=5><b>Holidays are not available</b></td></tr></table>");
+                        lblPGResult.InnerHtml = sb.ToString();
+                    }
+
+                }
+
+                catch (Exception ex)
+                {
+                    SPDiagnosticsService.Local.WriteTrace(0, new SPDiagnosticsCategory("Holidays - PageLoad", TraceSeverity.High, EventSeverity.Error), TraceSeverity.High, ex.Message, ex.StackTrace);
+
+                }
+            }
+        }
+
+        private void GetHolidays(string country, bool isCtype)
+        {
+            using (SPSite site = new SPSite(SPContext.Current.Web.Url))
+            {
+
+                using (SPWeb web = site.OpenWeb())
+                {
+                    SPList list = web.Lists.TryGetList("Holidays");
+                    if (list != null)
+                    {
+                        SPQuery qry = new SPQuery();
+                        string stringQuery = "";
+                        if (isCtype == true)
+                        {
+                            string strPG = "0";
+                            stringQuery = string.Format(@"<Where><And><And><Eq><FieldRef Name='Country' /><Value Type='Choice'>{0}</Value></Eq><Eq><FieldRef Name='IsPGHoliday' /><Value Type='bit'>{1}</Value></Eq></And><Eq><FieldRef Name='FiscalYear' /><Value Type='Lookup'>{2}</Value></Eq></And></Where>", country, strPG,DateTime.Now.Year);
+                        }
+                        else
+                        {
+                            string strPG = "1";
+                            stringQuery = string.Format(@"<Where><And><Eq><FieldRef Name='IsPGHoliday' /><Value Type='bit'>{0}</Value></Eq><Eq><FieldRef Name='FiscalYear' /><Value Type='Lookup'>{1}</Value></Eq></And></Where>", strPG, DateTime.Now.Year);
+                        }
+                        qry.Query = stringQuery;
+                        qry.ViewFields = @"<FieldRef Name='HolidayDate' /><FieldRef Name='Title' /><FieldRef Name='Comment' />";
+                        SPListItemCollection listcoll = list.GetItems(qry);
+                        DataTable dtHoliday = new DataTable();
+                        //dtHoliday.Columns.Add("SNo");
+                        dtHoliday.Columns.Add("Month");
+                        dtHoliday.Columns.Add("Day");
+                        dtHoliday.Columns.Add("HolidayDate");
+                        dtHoliday.Columns.Add("HolidayName");
+                        dtHoliday.Columns.Add("MonthNum",typeof(int));
+                        foreach (SPListItem item in listcoll)
+                        {
+                            DataRow row = dtHoliday.NewRow();
+                            string title = item["Title"].ToString();
+                            if (title.Contains("*"))
+                            {
+                               // lblComment.Visible = true;
+                                if (item["Comment"]!=null)
+                                {
+                                    Label lblTextComment = new Label();
+                                    lblTextComment.Text = "*"+item["Comment"].ToString();
+                                    pnlComment.Controls.Add(lblTextComment);
+                                }
+                                //lblComment.Text = item["Comment"].ToString();
+                            }
+                            string HDate = item["HolidayDate"].ToString();
+                            DateTime dt = DateTime.Parse(HDate);
+                            string HdateName = String.Format("{0:MMMM d, yyyy}", dt);
+                            string HolidayMonth = String.Format("{0:MMMM}", dt);
+                            int MonthNumber = Convert.ToInt32(dt.Month.ToString());
+                            string HolidayDay = String.Format("{0:dddd}", dt);
+                           // count += 1;
+                            //row["SNo"] = count.ToString();
+                            row["Month"] = HolidayMonth.ToString();
+                            row["Day"] = HolidayDay.ToString();
+                            row["HolidayDate"] = HdateName.ToString();
+                            row["HolidayName"] = title.ToString();
+                            row["MonthNum"] = MonthNumber;
+                            dtHoliday.Rows.Add(row);
+
+                        }
+
+                        if (dtHoliday.Rows.Count > 0)
+                        {
+                            DataView dv = dtHoliday.DefaultView;
+                            dv.Sort = "MonthNum";
+                            gvHolidays.DataSource = dv;
+                            gvHolidays.DataBind();
+                        }
+                        else {
+                            StringBuilder sb = new StringBuilder();
+
+                            sb.Append("<table style=Width:400px><tr Class=grid-sub1><td Class=grid-header>S.No</td><td  Class=grid-header>Month</td><td  Class=grid-header>Day</td><td Class=grid-header>Date</td><td Class=grid-header>Holidays</td></tr><tr  Class=grid-sub1><td colspan=5><b>Holidays are not available</b></td></tr></table>");
+                            lblPGResult.InnerHtml = sb.ToString();
+                        }
+
+                    }
+                }
+            }
+        }
+       
+
+       
+
+    }
+}
