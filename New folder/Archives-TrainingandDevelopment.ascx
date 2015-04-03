f2d0ffead163adaaf185c362090c73eae2bada63
dintakurthianil <anilkumar.d@inhycgd6166.vertexcs.com>
2015-04-03 08:07:42 -0700

    ArchivesTrainingandDevelopment
    ArchivesTrainingandDevelopment

---
+++ New folder/Archives-TrainingandDevelopment.ascx
@@ -0,0 +1,84 @@
+﻿<%@ Assembly Name="$SharePoint.Project.AssemblyFullName$" %>
+<%@ Assembly Name="Microsoft.Web.CommandUI, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+<%@ Register TagPrefix="SharePoint" Namespace="Microsoft.SharePoint.WebControls" Assembly="Microsoft.SharePoint, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+<%@ Register TagPrefix="Utilities" Namespace="Microsoft.SharePoint.Utilities" Assembly="Microsoft.SharePoint, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+<%@ Register TagPrefix="asp" Namespace="System.Web.UI" Assembly="System.Web.Extensions, Version=4.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35" %>
+<%@ Import Namespace="Microsoft.SharePoint" %>
+<%@ Register TagPrefix="WebPartPages" Namespace="Microsoft.SharePoint.WebPartPages" Assembly="Microsoft.SharePoint, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="Archives-TrainingandDevelopment.ascx.cs" Inherits="VrtxIntranetArchives.Archives_TrainingandDevelopment.Archives_TrainingandDevelopment" %>
+
+
+
+<SharePoint:CssRegistration runat="server" Name="/_layouts/15/VrtxIntranetStyles/CSS/intranet.css" />
+<div style="margin-top: 19px;">
+    <asp:GridView ID="gvTraining" runat="server" AutoGenerateColumns="false" Width="100%" BorderStyle="0" GridLines="None">
+        <AlternatingRowStyle CssClass="" BorderWidth="0" />
+        <RowStyle CssClass="grid-sub1" BorderWidth="0" />
+        <HeaderStyle CssClass="grid-header" />
+
+
+        <Columns>
+            <%--<asp:TemplateField HeaderText="S.No" HeaderStyle-HorizontalAlign="Left"  > 
+            <ItemTemplate>
+               <asp:Label ID="lblSerialNo" Text='<%#Container.DataItemIndex + 1%>'  runat="server"></asp:Label>
+            </ItemTemplate>
+        </asp:TemplateField>--%>
+            <asp:TemplateField HeaderText="Name of Training" HeaderStyle-HorizontalAlign="Left">
+                <ItemTemplate>
+                    <asp:Label ID="lblNameofTraining" Text='<%#Eval("Title")%>' runat="server"></asp:Label>
+                </ItemTemplate>
+            </asp:TemplateField>
+            <asp:TemplateField HeaderText="Target Audience" HeaderStyle-HorizontalAlign="Left">
+                <ItemTemplate>
+                    <asp:Label ID="lblTargetAudience" Text='<%#Eval("TargetAudience")%>' runat="server"></asp:Label>
+                </ItemTemplate>
+            </asp:TemplateField>
+            <asp:TemplateField HeaderText="Planned Date" HeaderStyle-HorizontalAlign="Left">
+                <ItemTemplate>
+                    <asp:Label ID="lblPlannedDate" Text='<%#Eval("PlannedDate")%>' runat="server"></asp:Label>
+                </ItemTemplate>
+            </asp:TemplateField>
+            <asp:TemplateField HeaderText="Total Duration" HeaderStyle-HorizontalAlign="Left">
+                <ItemTemplate>
+                    <asp:Label ID="lblTotalDuration" Text='<%#Eval("TotalDuration")%>' runat="server"></asp:Label>
+                </ItemTemplate>
+            </asp:TemplateField>
+            <asp:TemplateField HeaderText="Mode of Training" HeaderStyle-HorizontalAlign="Left">
+                <ItemTemplate>
+                    <asp:Label ID="lblModeofTraining" Text='<%#Eval("Modeoftraining")%>' runat="server"></asp:Label>
+                </ItemTemplate>
+            </asp:TemplateField>
+            <asp:TemplateField HeaderText="Actual Date of Training" HeaderStyle-HorizontalAlign="Left">
+                <ItemTemplate>
+                    <asp:Label ID="lblActualDateofTraining" Text='<%#Eval("ActualDateofTraining")%>' runat="server"></asp:Label>
+                </ItemTemplate>
+            </asp:TemplateField>
+            <asp:TemplateField HeaderText="Name of the Trainer" HeaderStyle-HorizontalAlign="Left">
+                <ItemTemplate>
+                    <asp:Label ID="lblNameoftheTrainer" Text='<%#Eval("NameoftheTrainer")%>' runat="server"></asp:Label>
+                </ItemTemplate>
+            </asp:TemplateField>
+            <asp:TemplateField HeaderText="Start Date" HeaderStyle-HorizontalAlign="Left">
+                <ItemTemplate>
+                    <asp:Label ID="lblEventDate" Text='<%#Eval("EventDate")%>' runat="server"></asp:Label>
+                </ItemTemplate>
+            </asp:TemplateField>
+            <asp:TemplateField HeaderText="End Date" HeaderStyle-HorizontalAlign="Left">
+                <ItemTemplate>
+                    <asp:Label ID="lblEndDate" Text='<%#Eval("EndDate")%>' runat="server"></asp:Label>
+                </ItemTemplate>
+            </asp:TemplateField>
+
+            <asp:TemplateField HeaderText="Comments" HeaderStyle-HorizontalAlign="Left">
+                <ItemTemplate>
+                    <asp:Label ID="lblComments" Text='<%#Eval("Comments")%>' runat="server"></asp:Label>
+                </ItemTemplate>
+            </asp:TemplateField>
+        </Columns>
+    </asp:GridView>
+
+    <div style="padding-left: 30%; padding-top: 10%;">
+        <h2>
+            <asp:Label Text="" ID="lblError" Visible="false" runat="server" /></h2>
+    </div>
+</div>
---
+++ New folder/Archives-TrainingandDevelopment.ascx.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.SharePoint;
+using System;
+using System.ComponentModel;
+using System.Data;
+using System.Text;
+using System.Web;
+using System.Web.UI.WebControls.WebParts;
+
+namespace VrtxIntranetArchives.Archives_TrainingandDevelopment
+{
+    [ToolboxItemAttribute(false)]
+    public partial class Archives_TrainingandDevelopment : WebPart
+    {
+        // Uncomment the following SecurityPermission attribute only when doing Performance Profiling on a farm solution
+        // using the Instrumentation method, and then remove the SecurityPermission attribute when the code is ready
+        // for production. Because the SecurityPermission attribute bypasses the security check for callers of
+        // your constructor, it's not recommended for production purposes.
+        // [System.Security.Permissions.SecurityPermission(System.Security.Permissions.SecurityAction.Assert, UnmanagedCode = true)]
+        public Archives_TrainingandDevelopment()
+        {
+        }
+
+        protected override void OnInit(EventArgs e)
+        {
+            base.OnInit(e);
+            InitializeControl();
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                if (HttpContext.Current.Request.QueryString["year"] != null)
+                {
+                    string Year = HttpContext.Current.Request.QueryString["year"].ToString();
+                    GetCalenderEvents(Year);
+                }
+                else
+                {
+                    string yeare = DateTime.Now.Year.ToString();
+                    GetCalenderEvents(yeare);
+                }
+
+            }
+        }
+
+        private void GetCalenderEvents(string year)
+        {
+
+
+            using (SPSite osite = new SPSite(SPContext.Current.Web.Url))
+            {
+                using (SPWeb oweb = osite.OpenWeb())
+                {
+                    SPQuery qry = new SPQuery();
+                    SPList olist = oweb.Lists.TryGetList("Training & Development Calendar");
+                    string currentYear = DateTime.Now.Year.ToString();
+                    if (year != "allyears")
+                    {
+
+                        qry.Query = "<Where><Eq><FieldRef Name='FiscalYear' /><Value Type='Text'>" + year + "</Value></Eq></Where>";
+
+
+                    }
+                    else
+                    {
+
+                        qry.Query = "<Where><Neq><FieldRef Name='FiscalYear' /><Value Type='Text'>" + currentYear + "</Value></Neq></Where>";
+                    }
+                    SPListItemCollection collListItems = olist.GetItems(qry);
+                    if (collListItems.Count > 0)
+                    {
+                        DataTable dt = new DataTable();
+                        dt.Columns.Add("Title", typeof(string));
+                        dt.Columns.Add("TargetAudience", typeof(string));
+                        dt.Columns.Add("PlannedDate", typeof(string));
+                        dt.Columns.Add("TotalDuration", typeof(string));
+                        dt.Columns.Add("Modeoftraining", typeof(string));
+                        dt.Columns.Add("ActualDateofTraining", typeof(string));
+                        dt.Columns.Add("NameoftheTrainer", typeof(string));
+                        dt.Columns.Add("EndDate", typeof(string));
+                        dt.Columns.Add("EventDate", typeof(string));
+                        dt.Columns.Add("Comments", typeof(string));
+
+
+
+                        foreach (SPListItem item in collListItems)
+                        {
+
+
+                            string[] delimit = new string[] { ";#" };
+
+                            DataRow dr = dt.NewRow();
+                            string[] stringarray = item["TargetAudience"].ToString().Split((delimit), StringSplitOptions.RemoveEmptyEntries);
+                            StringBuilder sb = new StringBuilder();
+                            for (int i = 0; i < stringarray.Length; i++)
+                            {
+
+                                if (i % 2 != 0)
+                                {
+                                    sb.Append(stringarray[i].ToString()+" ");
+                                }
+                            }
+                            dr["Title"] = item["Title"] != null ? item["Title"].ToString() : string.Empty;
+                            dr["TargetAudience"] = sb.ToString();// fieldUserValues.ToString();//item["TargetAudience"] != null ? item["TargetAudience"].ToString() : string.Empty;
+                            dr["PlannedDate"] = item["PlannedDate"] != null ? item["PlannedDate"].ToString() : string.Empty;
+                            dr["TotalDuration"] = item["TotalDuration"] != null ? item["TotalDuration"].ToString() : string.Empty;
+                            dr["Modeoftraining"] = item["Modeoftraining"] != null ? item["Modeoftraining"].ToString() : string.Empty;
+                            dr["ActualDateofTraining"] = item["ActualDateofTraining"] != null ? item["ActualDateofTraining"].ToString() : string.Empty;
+                            dr["NameoftheTrainer"] = item["NameoftheTrainer"] != null ? item["NameoftheTrainer"].ToString() : string.Empty;
+                            dr["EndDate"] = item["EndDate"] != null ? item["EndDate"].ToString() : string.Empty;
+                            dr["EventDate"] = item["EventDate"] != null ? item["EventDate"].ToString() : string.Empty;
+                            dr["Comments"] = item["Comments"] != null ? item["Comments"].ToString() : string.Empty;
+                            dt.Rows.Add(dr);
+                        }
+                        gvTraining.DataSource = dt;
+                        gvTraining.DataBind();
+                    }
+                    else
+                    {
+                        lblError.Visible = true;
+                        lblError.Text = "No Data Found";
+                    }
+                }
+            }
+        }
+    }
+}
