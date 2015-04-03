00ef6aaae46fa5e5bdcc7fe9357955b60d97b4b0
dintakurthianil <anilkumar.d@inhycgd6166.vertexcs.com>
2015-04-03 08:11:00 -0700

    EmployeeAchivements
    EmployeeAchivements

---
+++ New folder/EmployeeAchievements.ascx
@@ -0,0 +1,175 @@
+﻿<%@ Assembly Name="$SharePoint.Project.AssemblyFullName$" %>
+<%@ Assembly Name="Microsoft.Web.CommandUI, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+<%@ Register TagPrefix="SharePoint" Namespace="Microsoft.SharePoint.WebControls" Assembly="Microsoft.SharePoint, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+<%@ Register TagPrefix="Utilities" Namespace="Microsoft.SharePoint.Utilities" Assembly="Microsoft.SharePoint, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+<%@ Register TagPrefix="asp" Namespace="System.Web.UI" Assembly="System.Web.Extensions, Version=4.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35" %>
+<%@ Import Namespace="Microsoft.SharePoint" %>
+<%@ Register TagPrefix="WebPartPages" Namespace="Microsoft.SharePoint.WebPartPages" Assembly="Microsoft.SharePoint, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="EmployeeAchievements.ascx.cs" Inherits="VrtxIntranetPortal.EmployeeAchievements.EmployeeAchievements" %>
+
+
+
+
+
+
+
+
+<SharePoint:CssRegistration runat="server" Name="/_layouts/15/VrtxIntranetStyles/CSS/intranet.css" />
+
+<script type="text/javascript" >
+
+    function CheckReportsTo(source, arguments) {
+        if (aspnetForm.ctl00_ctl41_g_257a10db_160e_41de_b98e_8a6ae44da70d_pplContarctEmp_containerCell_downlevelTextBox.value == "")
+            arguments.IsValid = false;
+        else
+            arguments.IsValid = true;
+
+    }
+
+    function vlaiddatePeoplePickr()
+    {
+        debugger;
+        var sttPeople = "#" + "<%=pplContarctEmp.ClientID %>";
+        var sttPeopledior = $(sttPeople)
+        var strng = $('ctl00_ctl41_g_257a10db_160e_41de_b98e_8a6ae44da70d_pplContarctEmp_downlevelTextBox');
+        ctl00_ctl41_g_257a10db_160e_41de_b98e_8a6ae44da70d_pplContarctEmp_downlevelTextBox
+        if (strng.text() == "")
+        {
+            alert("Please enter user name");
+            return false;
+        }
+        
+        return true;
+      
+
+    }
+   
+    function validatedDropdown()
+    {
+        //debugger;
+        var strFoll = "#" + "<%=ddlTechnologies.ClientID %>";
+        if ($(strFoll).get(0).selectedIndex == 0) {
+            alert("Please select Technologies ");
+            $(strFoll).focus();
+            return false;
+        }
+        else { return true;}
+
+
+    }
+  
+    </script> 
+
+<div id="peoplePicker" runat="server" Visible="false">
+    <table>
+
+        <tr >
+            <td style="padding-left: 335px;"">
+                 <asp:Label Text="Select User: " ID="lblSelectuser" runat="server" />
+            </td>
+            <td style=" padding-bottom: 6px;">
+               
+                <SharePoint:PeopleEditor ID="pplContarctEmp" runat="server"  Height="21px"    Width="300px" MultiSelect="false" ValidatorEnabled="false" EnableBrowse="true"  
+                                AllowEmpty="false" />
+
+            </td>
+            <td>
+                <asp:Button id="peoplePickerbutton" Text="Submit" runat="server" OnClick="peoplePickerbutton_Click"  />
+            </td>
+        </tr>
+    </table> 
+    
+</div>
+<div id="peoplepickerByCertification" runat="server" visible="false">
+    <table align="center">
+        <tr>
+
+            <td>
+
+                <asp:Label Text="Technologies : " runat="server" />
+
+            </td>
+            <td>
+                <asp:DropDownList runat="server" ID="ddlTechnologies" OnSelectedIndexChanged="ddlTechnologies_OnSelectedIndexChanged" Width ="200px" AutoPostBack="true">
+                    
+                </asp:DropDownList>&nbsp;<span style ="color:red;font-size:17px">*</span>
+            </td>
+            <tr>
+            <td>
+
+                <asp:Label Text="Certifications : " runat="server" />
+
+            </td>
+            <td>
+                <asp:DropDownList runat="server" ID="ddlCertifications" Width ="200px">
+                    <asp:ListItem Text="Select" Value="0" /> 
+                </asp:DropDownList>
+            </td>
+            
+        </tr>
+        <tr>
+           <td colspan ="2">
+                 <asp:Button id="btnpeoplepickerByCertification" Text="Submit" runat="server" OnClick="btnpeoplepickerByCertification_Click"  OnClientClick ="return validatedDropdown() " />
+            </td>
+        </tr>
+    </table>
+</div>
+<asp:GridView ID="gvEmpAchievements" runat="server" AutoGenerateColumns="false" BorderStyle="None"   GridLines="None" Width ="100%" >
+
+    <Columns>
+        <asp:TemplateField HeaderText="" >
+            <ItemTemplate>
+
+                <table style="background-color: #f7f7f7; overflow: hidden;  margin-bottom: 10px; margin-left: 0px; " >
+                    <tr>
+                        <td>
+                            <table style="width:100%" >
+                                <tr>
+
+                                    <td>
+                                        
+                                    <a style="text-decoration: none;" href='<%# Eval("Title", "/SitePages/ODCServiceProfiles.aspx?username={0}") %>'>  <asp:Image ID="imgImage" runat="server"  Height="100" Width="100" ImageUrl='<%# Eval("Attachments")%>' /></a>
+                                    
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td style="text-align:center">
+
+                                        <asp:HyperLink ID="HyperLink2" NavigateUrl='<%# Eval("Title", "/SitePages/ODCServiceProfiles.aspx?username={0}") %>' Text='<%# Eval("Title")%>' Target="_blank" runat="server"></asp:HyperLink>
+                                        <%--<asp:Label ID="lblEmpName" Text='<%#Eval("Title")%>' runat="server"></asp:Label>--%>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <asp:Label ID="lblDesignation" Text='<%#Eval("Designation")%>' runat="server"></asp:Label>
+                                    </td>
+                                </tr>
+                            </table>
+                        </td>
+
+                        <td style="vertical-align:top;width:100%" >
+
+                            <div runat="server" id="htmlable" innerhtml='<%#Eval("htmlable")%>'></div>
+
+                        </td>
+                    </tr>
+                </table>
+
+            </ItemTemplate>
+        </asp:TemplateField>
+    </Columns>
+</asp:GridView>
+<table style="padding-left: 30%;padding-top: 10%;">
+    <tr>
+        <td>
+            <h2><asp:Label ID="errorMsg" runat="server" Visible ="false" style="" ></asp:Label></h2>
+        </td>
+        <td>
+            <h2><asp:Label ID="btnerrormsg" runat="server"  Visible ="false" style="padding-left: 30%;padding-top: 10%;"></asp:Label></h2>
+        </td>
+
+    </tr>
+
+</table>
+
+
---
+++ New folder/EmployeeAchievements.ascx.cs
@@ -0,0 +1,367 @@
+﻿using Microsoft.SharePoint;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Text;
+using System.Web.UI.WebControls.WebParts;
+using System.Linq;
+using Microsoft.SharePoint.Utilities;
+using System.Web.UI.WebControls;
+using System.Web;
+
+namespace VrtxIntranetPortal.EmployeeAchievements
+{
+    [ToolboxItemAttribute(false)]
+    public partial class EmployeeAchievements : WebPart
+    {
+        // Uncomment the following SecurityPermission attribute only when doing Performance Profiling on a farm solution
+        // using the Instrumentation method, and then remove the SecurityPermission attribute when the code is ready
+        // for production. Because the SecurityPermission attribute bypasses the security check for callers of
+        // your constructor, it's not recommended for production purposes.
+        // [System.Security.Permissions.SecurityPermission(System.Security.Permissions.SecurityAction.Assert, UnmanagedCode = true)]
+        public EmployeeAchievements()
+        {
+        }
+
+        protected override void OnInit(EventArgs e)
+        {
+            base.OnInit(e);
+            InitializeControl();
+        }
+        SPList EmployeeAchievmentslist = null;
+        SPList EmpBasicInfolist = null;
+        SPQuery LatesrQuery = new SPQuery();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            if (!Page.IsPostBack)
+            {
+
+                if (HttpContext.Current.Request.QueryString["year"] != null)
+                {
+                    string Year = HttpContext.Current.Request.QueryString["year"].ToString();
+                    GeEmployeDetails(Year);
+                }
+                else
+                {
+                    string yeare = DateTime.Now.Year.ToString();
+                    GeEmployeDetails(yeare);
+                }
+                BindTechnologies();
+
+            }
+
+        }
+
+
+
+        private void GeEmployeDetails(string year)
+        {
+            using (SPSite osite = new SPSite(SPContext.Current.Web.Url))
+            {
+                using (SPWeb oweb = osite.OpenWeb())
+                {
+                    EmployeeAchievmentslist = oweb.Lists.TryGetList("EmpCertifications");
+                    EmpBasicInfolist = oweb.Lists.TryGetList("EmpBasicInfo");
+
+                    string queryResult = Page.Request.QueryString["ctype"];//["kqry"];
+
+                    if (queryResult == "Latest")
+                    {
+                        DateTime Today = DateTime.Today;
+                        DateTime value = Today.AddDays(-90);
+                        string dtvalue = value.Date.ToString();
+
+                        string lstQry = string.Format(@"<Where><And><Gt><FieldRef Name='CertifiedDate' /><Value IncludeTimeValue='FALSE' Type='DateTime'>{0}</Value></Gt><Lt><FieldRef Name='CertifiedDate' /><Value IncludeTimeValue='FALSE' Type='DateTime'>{1}</Value></Lt></And></Where>", SPUtility.CreateISO8601DateTimeFromSystemDateTime(value), SPUtility.CreateISO8601DateTimeFromSystemDateTime(Today));
+                        LatesrQuery.Query = lstQry;
+                        GetAchivements(LatesrQuery, EmployeeAchievmentslist, EmpBasicInfolist);
+
+                    }
+                    else if (queryResult == "Certificate")
+                    {
+                        peoplepickerByCertification.Visible = true;
+                        //BindTechnologies();
+
+                    }
+                    else if (queryResult == "user")
+                    {
+                        peoplePicker.Visible = true;
+                    }
+                    else
+                    {
+                        DateTime Today = DateTime.Today;
+                        DateTime value = Today.AddDays(-365);
+                        string dtvalue = value.Date.ToString();
+                        string lstQry=string.Empty;
+                        if (year!=null)
+                        {
+                            lstQry = string.Format(@"<Where><Eq><FieldRef Name='FiscalYear' /><Value Type='Lookup'>" + year + "</Value></Eq></Where>");
+                        }
+                        else if(year=="allyears")
+                        {
+                            string currentyear = DateTime.Now.Year.ToString();
+                            lstQry = string.Format(@"<Where><Neq><FieldRef Name='FiscalYear' /><Value Type='Lookup'>" + currentyear + "</Value><Neq></Where>");
+                            
+                        }
+                        else 
+                        {
+                            lstQry = string.Format(@"<Where><And><Gt><FieldRef Name='CertifiedDate' /><Value IncludeTimeValue='FALSE' Type='DateTime'>{0}</Value></Gt><Lt><FieldRef Name='Created' /><Value IncludeTimeValue='FALSE' Type='DateTime'>{1}</Value></Lt></And></Where>", SPUtility.CreateISO8601DateTimeFromSystemDateTime(value), SPUtility.CreateISO8601DateTimeFromSystemDateTime(Today));
+                        }
+                        
+                        LatesrQuery.Query = lstQry;
+                        GetAchivements(LatesrQuery, EmployeeAchievmentslist, EmpBasicInfolist);
+                    }
+                }
+            }
+        }
+
+
+
+        protected void peoplePickerbutton_Click(object sender, EventArgs e)
+        {
+            using (SPSite osite = new SPSite(SPContext.Current.Web.Url))
+            {
+                using (SPWeb oweb = osite.OpenWeb())
+                {
+                    EmployeeAchievmentslist = oweb.Lists.TryGetList("EmpCertifications");
+                    EmpBasicInfolist = oweb.Lists.TryGetList("EmpBasicInfo");
+                }
+            }
+            if (pplContarctEmp.CommaSeparatedAccounts.ToString() != "")
+            {
+                btnerrormsg.Visible = false;
+                string vari = pplContarctEmp.CommaSeparatedAccounts.ToString().Split('\\')[1];
+                SPQuery LatesrQuery = new SPQuery();
+                string Loguser = vari.ToString();//loggedinuser.ToString().Split('\\')[1];
+                string stringQuery = string.Format(@"<Where><Eq><FieldRef Name='UserName' /><Value Type='Text'>{0}</Value></Eq></Where>", Loguser);
+                LatesrQuery.Query = stringQuery;
+                GetAchivements(LatesrQuery, EmployeeAchievmentslist, EmpBasicInfolist);
+                //pplContarctEmp.CommaSeparatedAccounts = "";
+                pplContarctEmp.Entities.Clear();
+                pplContarctEmp.ResolvedEntities.Clear();
+
+            }
+            else
+            {
+                gvEmpAchievements.Visible = false;
+                btnerrormsg.Visible = true;
+                btnerrormsg.Text = "Please enter Employee name";
+
+            }
+        }
+
+        private void BindTechnologies()
+        {
+            try
+            {
+                SPSecurity.RunWithElevatedPrivileges(delegate()
+                {
+                    using (SPSite site = new SPSite(SPContext.Current.Site.Url))
+                    {
+                        using (SPWeb web = site.OpenWeb())
+                        {
+                            SPList list = web.Lists["Technologies"];
+                            SPListItemCollection ocol = list.GetItems();
+                            ddlTechnologies.DataSource = ocol;
+                            ddlTechnologies.DataTextField = "Title"; //List field holding name
+                            //ddlTechnologies.DataValueField = "Title"; //List field holding value
+                            ddlTechnologies.DataBind();
+                            ddlTechnologies.Items.Insert(0, new ListItem("Select", "0"));
+                            //ViewState["ddlvalue"] = ddlTechnologies.SelectedItem.Text;
+
+                        }
+                    }
+                });
+            }
+            catch (Exception ex)
+            {
+                //Page.ClientScript.RegisterClientScriptBlock(typeof(SPAlert), "alert", "<script language=\"javascript\">alert('" + ex.Message + "')</script>"); 
+            }
+        }
+
+        protected void ddlTechnologies_OnSelectedIndexChanged(object sender, EventArgs e)
+        {
+            ddlCertifications.Items.Clear();
+            gvEmpAchievements.Visible = false;
+
+            try
+            {
+                SPSecurity.RunWithElevatedPrivileges(delegate()
+                {
+                    using (SPSite site = new SPSite(SPContext.Current.Site.Url))
+                    {
+                        using (SPWeb web = site.OpenWeb())
+                        {
+                            SPList list = web.Lists["Certifications"];
+                            SPQuery ddlquery = new SPQuery();
+                            string quervalue = ddlTechnologies.SelectedItem.Text;
+                            string quey = string.Format(@"<Where><Eq><FieldRef Name='Technology' /><Value Type='Lookup'>{0}</Value></Eq></Where>", quervalue);
+                            ddlquery.Query = quey;
+                            SPListItemCollection ocol = list.GetItems(ddlquery);
+                            ddlCertifications.DataSource = ocol;
+                            ddlCertifications.DataTextField = "Title"; //List field holding name
+                            //ddlCertifications.DataValueField = "Title"; //List field holding value
+                            ddlCertifications.DataBind();
+                            ddlCertifications.Items.Insert(0, new ListItem("Select", "0"));
+
+                        }
+                    }
+                });
+            }
+            catch (Exception ex)
+            {
+                //Page.ClientScript.RegisterClientScriptBlock(typeof(SPAlert), "alert", "<script language=\"javascript\">alert('" + ex.Message + "')</script>");
+            }
+
+        }
+
+        private void GetAchivements(SPQuery query, SPList EmployeeAchievmentslist, SPList EmpBasicInfolist)
+        {
+            SPListItemCollection collListItems = EmployeeAchievmentslist.GetItems(query);
+
+
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Attachments", typeof(string));
+            dt.Columns.Add("Title", typeof(string));
+            dt.Columns.Add("Designation", typeof(string));
+            dt.Columns.Add("htmlable");
+            StringBuilder sb = new StringBuilder();
+            if (collListItems.Count > 0)
+            {
+                List<string> strlist = new List<string>();
+                foreach (SPListItem item in collListItems)
+                {
+                    strlist.Add(item["UserName"] != null ? item["UserName"].ToString() : string.Empty);
+                    string strVal = string.Join(",", strlist.ToArray());
+                }
+                var result = (from m in strlist select m).Distinct().ToList();//it  is used to remove duplicates
+
+                sb.Append("<table style='width:100%;' cellspacing='0' >");
+                sb.Append("<tr Style=background-color:#c3d9eb;height:20px;font-size:14px;font-weight:bold;>");
+                sb.Append("<td style='width:5%;padding: 15px;' ></td>");
+                sb.Append("<td style='width:5%;'>S No</td>");
+                sb.Append("<td style='width:15%;padding: 5px;'>Technologies</td>");
+                sb.Append("<td style='width:19%;padding: 5px;' >Certifications</td>");
+                sb.Append("<td style='width:19%;padding: 5px;'>Certification ID</td>");
+                sb.Append("<td style='width:19%;padding: 5px;'>Certified Date</td>");
+                sb.Append("<td style='width:19%;padding: 5px;'>Expiry Date</td>");
+                sb.Append("</tr>");
+                foreach (string user in result)
+                {
+                    DataRow dr = dt.NewRow();
+                    SPQuery q = new SPQuery();
+                    string stringQuery = string.Format(@"<Where><Eq><FieldRef Name='Title' /><Value Type='Text'>{0}</Value></Eq></Where>", user);
+                    q.Query = stringQuery;
+                    SPListItemCollection ocollListItems = EmpBasicInfolist.GetItems(q);
+
+                    foreach (SPListItem items in ocollListItems)
+                    {
+
+                        string attachmentAbsoluteURL = string.Empty;
+                        if (items.Attachments.Count > 0)
+                        {
+                            foreach (String attachmentname in items.Attachments)
+                            {
+                                attachmentAbsoluteURL = items.Attachments.UrlPrefix + attachmentname;
+                                dr["Attachments"] = attachmentAbsoluteURL.ToString();
+                            }
+                        }
+                        else
+                        {
+                            dr["Attachments"] = "/_layouts/15/VrtxIntranetStyles/Images/NoImage.jpg";
+                        }
+                        dr["Title"] = items["Title"] != null ? items["Title"].ToString() : string.Empty;
+                        dr["Designation"] = items["Designation"] != null ? items["Designation"].ToString().Split('#')[1] : string.Empty;
+
+                        SPQuery qry = new SPQuery();
+                        if (query != null)
+                        {
+                            collListItems = EmployeeAchievmentslist.GetItems(query);
+                        }
+                        else
+                        {
+
+                            qry.Query = "<Where><Eq><FieldRef Name='UserName' /><Value Type='Text'>" + user + "</Value></Eq></Where>";
+                            collListItems = EmployeeAchievmentslist.GetItems(qry);
+                        }
+
+
+                        string filepath = "/_layouts/15/VrtxIntranetStyles/Images/achievement-icon.jpg";
+                        int i = 1;
+                        dr["htmlable"] = sb.Append("<table cellspacing='0' style='width:100%'>");
+                        foreach (SPListItem item in collListItems)
+                        {
+                            if (item["UserName"].ToString() == user)
+                            {
+
+                                string Technologies = item["Technology"].ToString().Split('#')[1];
+                                string Certifications = item["Certificate"].ToString().Split('#')[1];
+                                string CertificationID = item["CertificationID"] != null ? item["CertificationID"].ToString() : string.Empty;
+
+                                string CertifiedDate = item["CertifiedDate"] != null ? item["CertifiedDate"].ToString().Split(' ')[0] : string.Empty;
+                                string UntillDate = item["UntilDate"] != null ? item["UntilDate"].ToString().Split(' ')[0] : string.Empty;
+                                sb.Append("<tr>");
+                                sb.AppendFormat("<td style='width:5%;padding: 5px;' >{0}</td>", "<img src='" + filepath + "'/>");
+                                sb.AppendFormat("<td style='width:5%;padding: 5px;'>{0}</td>", i);
+                                sb.AppendFormat("<td style='width:15%;padding: 5px;'>{0}</td>", Technologies);
+                                sb.AppendFormat("<td style='width:19%;padding: 5px;'>{0}</td>", Certifications);
+                                sb.AppendFormat("<td style='width:19%;padding: 5px;'>{0}</td>", CertificationID);
+                                sb.AppendFormat("<td style='width:19%;padding: 5px;'>{0}</td>", CertifiedDate);
+                                sb.AppendFormat("<td style='width:19%;padding: 5px;'>{0}</td>", UntillDate);
+                                sb.Append("<tr/>");
+                                i++;
+                            }
+                        }
+                        dr["htmlable"] = sb.Append("</table>");
+                        sb.Clear();
+                        dt.Rows.Add(dr);
+                    }
+
+                    gvEmpAchievements.Visible = true;
+                    errorMsg.Visible = false;
+                    gvEmpAchievements.DataSource = dt;
+                    gvEmpAchievements.DataBind();
+
+                }
+            }
+
+            else
+            {
+                gvEmpAchievements.Visible = false;
+                errorMsg.Visible = true;
+                errorMsg.Text = "No Data Found ";
+            }
+        }
+
+        protected void btnpeoplepickerByCertification_Click(object sender, EventArgs e)
+        {
+
+
+            using (SPSite osite = new SPSite(SPContext.Current.Web.Url))
+            {
+                using (SPWeb oweb = osite.OpenWeb())
+                {
+                    EmployeeAchievmentslist = oweb.Lists.TryGetList("EmpCertifications");
+                    EmpBasicInfolist = oweb.Lists.TryGetList("EmpBasicInfo");
+
+                    if (ddlTechnologies.SelectedIndex > 0 && ddlCertifications.SelectedIndex > 0)
+                    {
+                        string lstQry = string.Format("@<Where><And><Eq><FieldRef Name='Technology' /><Value Type='Lookup'>{0}</Value></Eq><Eq><FieldRef Name='Certificate' /><Value Type='Lookup'>{1}</Value></Eq></And></Where>", ddlTechnologies.SelectedItem.Text, ddlCertifications.SelectedItem.Text);//("@<Where><And><Eq><FieldRef Name='Technologies' /><Value Type='Lookup'>{0}</Value></Eq><Eq><FieldRef Name='Certifications' /><Value Type='Lookup'>{1}</Value></Eq></And></Where>", ddlTechnologies.SelectedItem.Text, ddlCertifications.SelectedItem.Text);
+
+                        LatesrQuery.Query = lstQry;
+                        GetAchivements(LatesrQuery, EmployeeAchievmentslist, EmpBasicInfolist);
+                    }
+                    else if (ddlTechnologies.SelectedIndex > 0)
+                    {
+                        string lstQry = string.Format("@<Where><Eq><FieldRef Name='Technology' /><Value Type='Lookup'>{0}</Value></Eq></Where>", ddlTechnologies.SelectedItem.Text);
+                        LatesrQuery.Query = lstQry;
+                        GetAchivements(LatesrQuery, EmployeeAchievmentslist, EmpBasicInfolist);
+                    }
+
+
+                }
+            }
+        }
+    }
+}
