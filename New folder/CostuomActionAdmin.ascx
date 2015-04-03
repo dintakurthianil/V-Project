ecc3bac0d311f50a8d428ea20974fbeaeeefa616
dintakurthianil <anilkumar.d@inhycgd6166.vertexcs.com>
2015-04-03 08:09:49 -0700

    CoustomActionAdmin
    CoustomActionAdmin

---
+++ New folder/CostuomActionAdmin.ascx
@@ -0,0 +1,83 @@
+﻿<%@ Assembly Name="$SharePoint.Project.AssemblyFullName$" %>
+<%@ Assembly Name="Microsoft.Web.CommandUI, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+<%@ Register TagPrefix="SharePoint" Namespace="Microsoft.SharePoint.WebControls" Assembly="Microsoft.SharePoint, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+<%@ Register TagPrefix="Utilities" Namespace="Microsoft.SharePoint.Utilities" Assembly="Microsoft.SharePoint, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+<%@ Register TagPrefix="asp" Namespace="System.Web.UI" Assembly="System.Web.Extensions, Version=3.5.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35" %>
+<%@ Import Namespace="Microsoft.SharePoint" %>
+<%@ Register TagPrefix="WebPartPages" Namespace="Microsoft.SharePoint.WebPartPages" Assembly="Microsoft.SharePoint, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="CostuomActionAdmin.ascx.cs" Inherits="SuiteBarBrandingDelegate.ControlTemplates.SuiteBarBrandingDelegate.CostuomActionAdmin" %>
+
+<script type="text/javascript" src="/_layouts/15/VrtxIntranetStyles/JS/jquery-1.8.2.js"></script>
+<script type="text/javascript" src="/_layouts/15/VrtxIntranetStyles/JS/jquery.SPServices-0.7.2.min.js"></script>
+
+
+<script>
+        function MYProfile() {
+        var urlvalue = "../SitePages/EmpInfo.aspx";
+        window.location = urlvalue;
+    }
+</script>
+<script type="text/javascript">
+    var loggedinUserGroup;
+    debugger;
+    $(document).ready(function () {
+        Getrolesforuser();
+        
+    });
+
+    function Getrolesforuser() {
+        var strDescrition = "<%=administration.ClientID %>";
+        $("#" + strDescrition).hide();
+       
+        loggedinUserGroup = "";
+        $().SPServices({
+            operation: "GetGroupCollectionFromUser",
+            userLoginName: $().SPServices.SPGetCurrentUser(),
+            async: false,
+            completefunc: function (xData, Status) {
+                $(xData.responseXML).find("Group").each(function () {
+                    loggedinUserGroup = $(this).attr("Name");
+                   
+                    
+                    
+                    if (loggedinUserGroup == "HR") {
+                        
+
+
+                        $("#" + strDescrition).show();
+                        
+                    }
+                    else
+                    {
+                        $("#" + strDescrition).hide();
+                    }
+                    if (loggedinUserGroup == "Marketing") {
+
+
+
+                        $("#" + strDescrition).show();
+
+                    }
+                    else {
+                        $("#" + strDescrition).hide();
+                    }
+                    //if (loggedinUserGroup == "Proprietários do Site") {
+                    //    $("#ms-designer-ribbon").hide();
+                    //}
+                });
+            }
+        });
+    }
+
+</script> 
+
+
+
+
+<a href="/SitePages/EmpInfo.aspx" id="myprofile" runat="server" style="cursor:pointer;"><img src="/_layouts/15/VrtxIntranetStyles/Images/my-profile.png" style=" vertical-align: middle" alt="Smiley face"></a>&nbsp;&nbsp;
+
+<asp:LinkButton   href="/Lists/Admin%20Links/AllItems.aspx"    id="administration" runat="server" target="_blank" style="cursor:pointer; " >
+<img src="/_layouts/15/VrtxIntranetStyles/Images/admin.png" style=" vertical-align: middle" alt="Smiley face" >
+</asp:LinkButton>
+<%--<a href="/Lists/Admin%20Links/AllItems.aspx" visible="false"  id="aadministration"  runat="server" target="_blank" style="cursor:pointer; "><img src="/_layouts/15/VrtxIntranetStyles/Images/admin.png" style=" vertical-align: middle"  alt="Smiley face" >
+</a>&nbsp;--%>
---
+++ New folder/CostuomActionAdmin.ascx.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.Utilities;
+using Microsoft.SharePoint.WebControls;
+using System.Globalization;
+using System.IO;
+using System.Collections;
+using Microsoft.SharePoint.Portal;
+using Microsoft.SharePoint.Portal.WebControls;
+
+namespace SuiteBarBrandingDelegate.ControlTemplates.SuiteBarBrandingDelegate
+{
+    public partial class CostuomActionAdmin : UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //checkuser();
+            //CheckIfUserIsAGroupMember("HR");
+        }
+
+
+        //protected override void Render(HtmlTextWriter writer)
+        //{
+        //    writer.RenderBeginTag(HtmlTextWriterTag.Style);
+        //    writer.Write(".ms-promotedActionButton {display: inline-block;}");
+        //    writer.RenderEndTag();
+        //    writer.AddAttribute(HtmlTextWriterAttribute.Class, "ms-promotedActionButton");
+        //    writer.RenderBeginTag(HtmlTextWriterTag.A);
+        //    AddPromotedSuiteLink(writer, "http://uscl2012v3:1234/SitePages/Adminpage.aspx", "#", "Administration", "Fb_Lsp");
+        //    writer.RenderEndTag();
+        //    base.Render(writer);
+
+        //}
+        //protected static void AddPromotedSuiteLink(HtmlTextWriter writer, string url, string iconurl, string name, string linkId)
+        //{
+        //    writer.AddAttribute(HtmlTextWriterAttribute.Class, "ms-promotedActionButton");
+
+        //    writer.AddAttribute(HtmlTextWriterAttribute.Title, name);
+        //    writer.AddAttribute(HtmlTextWriterAttribute.Onclick, url);
+
+        //    writer.RenderBeginTag(HtmlTextWriterTag.Span);
+        //    writer.AddAttribute(HtmlTextWriterAttribute.Class, "s4-clust ms-promotedActionButton-icon");
+
+        //    writer.RenderBeginTag(HtmlTextWriterTag.Img);
+        //    writer.AddAttribute(HtmlTextWriterAttribute.Src, iconurl);
+        //    writer.RenderEndTag();
+
+        //    writer.RenderEndTag();
+
+        //    writer.RenderBeginTag(HtmlTextWriterTag.Span);
+        //    writer.AddAttribute(HtmlTextWriterAttribute.Class, "ms-promotedActionButton-text");
+
+        //    writer.AddAttribute(HtmlTextWriterAttribute.Id, linkId);
+
+        //    writer.Write(name);
+        //    writer.RenderEndTag();
+        //}
+
+        //private void checkusegroup()
+        //{
+        //    using (SPSite site = new SPSite(SPContext.Current.Web.Url))
+        //    {
+        //        using (SPWeb objWeb = site.OpenWeb())
+        //        {
+        //            SPList oList = objWeb.Lists["Kaizen Category"];
+
+        //            SPListItemCollection results = oList.GetItems();
+
+        //            foreach (SPListItem item in results)
+        //            {
+
+        //                string group = item["Category"].ToString();
+
+        //                SPGroup groupName = SPContext.Current.Web.Groups[group];////.Split('#')[1] 
+
+        //                SPUser user = objWeb.EnsureUser(objWeb.CurrentUser.ToString());
+
+        //                if (groupName.ContainsCurrentUser)
+        //                {
+        //                    //divDrop.Visible = true;
+
+        //                }
+        //            }
+        //        }
+        //    }
+
+        //}
+
+        #region May be use later
+        //public bool CheckIfUserIsAGroupMember(string groupName)
+        //{
+        //    using (SPSite site = new SPSite(SPContext.Current.Site.ID))
+        //    {
+        //        using (SPWeb web = site.OpenWeb(SPContext.Current.Web.ID))
+        //        {
+        //            return web.SiteGroups[groupName].ContainsCurrentUser;
+        //        }
+        //    }
+        //} 
+        #endregion
+        private void checkuser()
+        {
+            using (SPSite ostite = SPContext.Current.Site)
+            {
+                using (SPWeb rootWeb = ostite.OpenWeb())
+                {
+                    //foreach (SPGroup group in rootWeb.SiteGroups)
+                    //{
+                        SPGroup Groupname;
+                        bool IsHe;
+                        SPUser currentuser = SPContext.Current.Web.CurrentUser;
+                        //Checking Logged in user belong to HR
+                        Groupname = rootWeb.Groups["HR"];
+                        IsHe = isUserInGroup(Groupname, currentuser.LoginName);
+                        if (IsHe == true)
+                        {
+                            administration.Visible = true;
+                        }
+
+
+                        //Checking Logged in user belong to Marketing
+                        Groupname = rootWeb.Groups["Marketing"];
+                        IsHe = isUserInGroup(Groupname, currentuser.LoginName);
+                        if (IsHe == true)
+                        {
+                            administration.Visible = true;
+                        }
+
+
+                    //}
+                }
+            }
+
+        }
+
+        private Boolean isUserInGroup(SPGroup oGroupName, String sUserLoginName)
+        {
+            Boolean bUserIsInGroup = false;
+            try
+            {
+                SPUser x = oGroupName.Users[sUserLoginName];
+                bUserIsInGroup = true;
+            }
+            catch (SPException)
+            {
+                bUserIsInGroup = false;
+            }
+            return bUserIsInGroup;
+
+        }
+
+       
+
+    }
+}
+
+
+
+
+
