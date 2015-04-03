ef0c330de6e2a392d64cd377517e1fb51e20c6d5
dintakurthianil <anilkumar.d@inhycgd6166.vertexcs.com>
2015-04-03 08:14:49 -0700

    HRNewsLetters

---
+++ New folder/HRNewsletter.ascx
@@ -0,0 +1,40 @@
+﻿<%@ Assembly Name="$SharePoint.Project.AssemblyFullName$" %>
+<%@ Assembly Name="Microsoft.Web.CommandUI, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+<%@ Register TagPrefix="SharePoint" Namespace="Microsoft.SharePoint.WebControls" Assembly="Microsoft.SharePoint, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+<%@ Register TagPrefix="Utilities" Namespace="Microsoft.SharePoint.Utilities" Assembly="Microsoft.SharePoint, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+<%@ Register TagPrefix="asp" Namespace="System.Web.UI" Assembly="System.Web.Extensions, Version=4.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35" %>
+<%@ Import Namespace="Microsoft.SharePoint" %>
+<%@ Register TagPrefix="WebPartPages" Namespace="Microsoft.SharePoint.WebPartPages" Assembly="Microsoft.SharePoint, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="HRNewsletter.ascx.cs" Inherits="TalentManagement.HRNewsletter.HRNewsletter" %>
+
+<SharePoint:CssRegistration runat="server" Name="/_layouts/15/VrtxIntranetStyles/CSS/intranet.css" />
+
+<div style="margin-top:19px;">
+    <asp:GridView ID="grdDocument" runat="server" AutoGenerateColumns="false" Style="width: 100%" GridLines="None">
+        <AlternatingRowStyle CssClass="grid-sub2" BorderWidth="0" />
+        <RowStyle CssClass="grid-sub1" BorderWidth="0" />
+        <HeaderStyle CssClass="grid-header" />
+        <Columns>
+            <asp:TemplateField HeaderText="S.NO" HeaderStyle-HorizontalAlign="Left">
+                <ItemTemplate>
+                    <asp:Label ID="lblIndiaSerialNo" Text='<%#Container.DataItemIndex + 1%>' Width="50px" runat="server"></asp:Label>
+                </ItemTemplate>
+            </asp:TemplateField>
+            <asp:TemplateField HeaderText=" Month" HeaderStyle-HorizontalAlign="Left">
+                <ItemTemplate>
+                    <asp:Label ID="Label1" Text='<%#Eval("Month")%>' runat="server"></asp:Label>
+                </ItemTemplate>
+            </asp:TemplateField>
+            <asp:TemplateField HeaderText="DownLoad Link" HeaderStyle-HorizontalAlign="Left">
+                <ItemTemplate>
+                    <a href='<%#Eval("URl")%>' style="vertical-align: top"><%#"Click Here to download" +" "+ Eval("Month") + " " + "News letter"%></a>
+                </ItemTemplate>
+            </asp:TemplateField>
+        </Columns>
+    </asp:GridView>
+
+    <div style="padding-left: 30%; padding-top: 10%;">
+        <h2>
+            <asp:Label Text="" ID="lblError" Visible="false" runat="server" /></h2>
+    </div>
+</div>
---
+++ New folder/HRNewsletter.ascx.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.SharePoint;
+using System;
+using System.ComponentModel;
+using System.Data;
+using System.Web;
+using System.Web.UI.WebControls.WebParts;
+
+namespace TalentManagement.HRNewsletter
+{
+    [ToolboxItemAttribute(false)]
+    public partial class HRNewsletter : WebPart
+    {
+        // Uncomment the following SecurityPermission attribute only when doing Performance Profiling on a farm solution
+        // using the Instrumentation method, and then remove the SecurityPermission attribute when the code is ready
+        // for production. Because the SecurityPermission attribute bypasses the security check for callers of
+        // your constructor, it's not recommended for production purposes.
+        // [System.Security.Permissions.SecurityPermission(System.Security.Permissions.SecurityAction.Assert, UnmanagedCode = true)]
+        public HRNewsletter()
+        {
+        }
+
+        protected override void OnInit(EventArgs e)
+        {
+            base.OnInit(e);
+            InitializeControl();
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (HttpContext.Current.Request.QueryString["year"] != null)
+            {
+                string Year = HttpContext.Current.Request.QueryString["year"].ToString();
+                GetHRNewsletters(Year);
+            }
+            else
+            {
+                string yeare = DateTime.Now.Year.ToString();
+                GetHRNewsletters(yeare);
+            }
+
+        }
+        private void GetHRNewsletters(string year)
+        {
+            using (SPSite osite = new SPSite(SPContext.Current.Web.Url))
+            {
+                using (SPWeb oweb = osite.OpenWeb())
+                {
+                    string currentYears = year;// DateTime.Now.Year.ToString();
+                    DataTable dtListItem = new DataTable();
+                    dtListItem.Columns.Add("Title", typeof(string));
+                    dtListItem.Columns.Add("URl", typeof(string));
+                    dtListItem.Columns.Add("Month", typeof(string));
+                    //dtListItem.Columns.Add("Month", typeof(string));
+                    string stringQueryclienttestmonial = string.Empty;
+                    SPList olist = oweb.Lists.TryGetList("HRNewsletter");
+                    SPQuery qurryclienttestmonialcollection = new SPQuery();
+                    if (currentYears != "allyears")
+                    {
+                        stringQueryclienttestmonial = string.Format(@"<Where><Eq><FieldRef Name='fiscalYear' /><Value Type='Lookup'>{0}</Value></Eq></Where>", currentYears);
+
+                    }
+                    else
+                    {
+                        string yearval = DateTime.Now.Year.ToString();
+                        stringQueryclienttestmonial = string.Format(@"<Where><Neq><FieldRef Name='fiscalYear' /><Value Type='Lookup'>" + yearval + "</Value></Neq></Where>");
+                    }
+                    qurryclienttestmonialcollection.Query = stringQueryclienttestmonial;
+                    SPListItemCollection col = olist.GetItems(qurryclienttestmonialcollection);
+                    if (col.Count > 0)
+                    {
+
+                        foreach (SPListItem item in col)
+                        {
+                            DataRow row = dtListItem.NewRow();
+
+                            row["URl"] = item["ows_FileRef"].ToString();
+                            row["Title"] = item["ows_LinkFilename"].ToString();
+                            row["Month"] = item["Month"].ToString();
+                            dtListItem.Rows.Add(row);
+
+                        }
+
+                        grdDocument.DataSource = dtListItem;
+                        grdDocument.DataBind();
+                    }
+                    else
+                    {
+                        lblError.Visible = true;
+                        lblError.Text = "No Data Found";
+                    }
+                }
+            }
+        }
+    }
+}
