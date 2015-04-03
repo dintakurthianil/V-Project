286ad8928cc142f91083aa13bc1a47a3fd7f11d2
dintakurthianil <anilkumar.d@inhycgd6166.vertexcs.com>
2015-04-03 08:16:11 -0700

    MoreEvents
    MoreEvents

---
+++ New folder/MoreEvents.ascx
@@ -0,0 +1,159 @@
+﻿<%@ Assembly Name="$SharePoint.Project.AssemblyFullName$" %>
+<%@ Assembly Name="Microsoft.Web.CommandUI, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+<%@ Register TagPrefix="SharePoint" Namespace="Microsoft.SharePoint.WebControls" Assembly="Microsoft.SharePoint, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+<%@ Register TagPrefix="Utilities" Namespace="Microsoft.SharePoint.Utilities" Assembly="Microsoft.SharePoint, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+<%@ Register TagPrefix="asp" Namespace="System.Web.UI" Assembly="System.Web.Extensions, Version=4.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35" %>
+<%@ Import Namespace="Microsoft.SharePoint" %>
+<%@ Register TagPrefix="WebPartPages" Namespace="Microsoft.SharePoint.WebPartPages" Assembly="Microsoft.SharePoint, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="MoreEvents.ascx.cs" Inherits="VrtxIntranetPortal.MoreEvents.MoreEvents" %>
+
+<SharePoint:CssRegistration runat="server" Name="/_layouts/15/VrtxIntranetStyles/CSS/intranet.css" />
+<%--<script src="/_layouts/15/VrtxIntranetStyles/JS/jquery-1.8.1.js"></script>--%>
+<style>
+    .events-ctr {
+        background-color: #f7f6f6;
+        overflow: hidden;
+        padding: 10px;
+        margin-bottom: 10px;
+    }
+
+    .events-pic-ctr {
+        border-right: #fff solid 1px;
+        width: 285px;
+        height: 165px;
+        margin-right: 10px;
+        float: left;
+    }
+
+
+    .events-des-ctr {
+        width: 510px;
+        float: left;
+        height: 160px;
+        padding: 0;
+        margin: 0;
+    }
+
+        .events-des-ctr p {
+            padding: 0;
+            margin: 0;
+            line-height: 20px;
+            height: 105px;
+        }
+
+    button-gallery {
+        font-size: 11px;
+        font-weight: bold;
+        line-height: 16px;
+        color: #000;
+        text-align: center;
+        text-transform: uppercase;
+        margin: 10px 0 0 0;
+        padding: 0;
+        position: relative;
+        float: left;
+    }
+
+    .button-gallery a {
+        background: #f16530;
+        color: #fff;
+        text-decoration: none;
+        margin: 0;
+        padding: 8px;
+    }
+
+        .button-gallery a:hover {
+            background: #133d8c;
+            color: #FFF;
+            text-decoration: none;
+            margin: 0;
+            padding: 8px;
+        }
+
+    .title-color {
+        color: #133d8c;
+    }
+
+    div#container {
+    }
+
+    a {
+        color: #EB067B;
+    }
+
+    div#pop-up {
+        display: none;
+        position: absolute;
+        width: 280px;
+        padding: 10px;
+        background: #eeeeee;
+        color: #000000;
+        border: 1px solid #1a1a1a;
+        font-size: 90%;
+    }
+</style>
+<div id="container">
+    <asp:GridView runat="server" ID="gridImage" AutoGenerateColumns="false" Width="850">
+
+
+        <Columns>
+            <asp:TemplateField>
+                <ItemTemplate>
+                    <div class="events-ctr">
+                        <div class="events-pic-ctr">
+                            <img src='<%#DataBinder.Eval(Container.DataItem, "ImagePath")%>' width="285" height="165" />
+                        </div>
+                        <div class="events-des-ctr">
+                            <asp:Label ID="Label2" runat="server" Font-Bold="true" Font-Size="15px" Text='<%# DataBinder.Eval(Container.DataItem, "Title") %>' CssClass="title-color"> </asp:Label>
+                            <div style="height: 128px; line-height: 20px;" id="Label1"  runat="server" innerhtml='<%# DataBinder.Eval(Container.DataItem, "Description") %>'></div>
+                            <div class="button-gallery"><a href='<%# DataBinder.Eval(Container.DataItem, "pageURL") %>' id="hrPhoto" target="_blank">Photo Gallery</a></div>
+                        </div>
+                    </div>
+                    </div>
+ 
+                </ItemTemplate>
+            </asp:TemplateField>
+        </Columns>
+    </asp:GridView>
+    <div id="pop-up">
+    </div>
+</div>
+<div style="padding-left: 30%;padding-top: 10%;">
+    <h2><asp:Label Text="" ID="lblError" Visible="false" runat="server" /></h2>
+</div>
+<div id="renderHtml" runat="server" style="display: none"></div>
+<script>
+
+
+
+
+
+    $(function () {
+        var moveLeft = 20;
+        var moveDown = 10;
+
+        $('a#trigger').hover(function (e) {
+            $('div#pop-up').show()
+      .css('top', e.pageY + moveDown)
+      .css('left', e.pageX + moveLeft - 300)
+      .appendTo('body');
+        }, function () {
+            $('div#pop-up').hide();
+        });
+
+
+
+    });
+
+    function mouseoveritem(thml) {
+        // alert("thml")
+        $('div#pop-up').html(thml);
+    }
+</script>
+
+
+
+
+
+
+
---
+++ New folder/MoreEvents.ascx.cs
@@ -0,0 +1,165 @@
+﻿using Microsoft.SharePoint;
+using System;
+using System.ComponentModel;
+using System.Web.UI.WebControls.WebParts;
+using System.Data;
+using System.IO;
+using System.Text;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.Administration;
+using System.Web;
+
+namespace VrtxIntranetPortal.MoreEvents
+{
+    [ToolboxItemAttribute(false)]
+    public partial class MoreEvents : WebPart
+    {
+        // Uncomment the following SecurityPermission attribute only when doing Performance Profiling on a farm solution
+        // using the Instrumentation method, and then remove the SecurityPermission attribute when the code is ready
+        // for production. Because the SecurityPermission attribute bypasses the security check for callers of
+        // your constructor, it's not recommended for production purposes.
+        // [System.Security.Permissions.SecurityPermission(System.Security.Permissions.SecurityAction.Assert, UnmanagedCode = true)]
+        public MoreEvents()
+        {
+        }
+
+        protected override void OnInit(EventArgs e)
+        {
+            base.OnInit(e);
+            InitializeControl();
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                if (HttpContext.Current.Request.QueryString["year"] != null)
+                {
+                    string Year = HttpContext.Current.Request.QueryString["year"].ToString();
+                    GetMoreEvents(Year);
+                }
+                else
+                {
+                    string yeare = DateTime.Now.Year.ToString();
+                    GetMoreEvents(yeare);
+                }
+
+            }
+        }
+
+
+
+        private void GetMoreEvents(string year)
+        {
+
+            using (SPSite site = new SPSite(SPContext.Current.Web.Url))
+            {
+                using (SPWeb web = site.OpenWeb())
+                {
+                    SPQuery qry = new SPQuery();
+                    SPList list = web.Lists.TryGetList("WhatsNew");
+                    string currenyear = DateTime.Now.Year.ToString();
+                    if (year != "allyears")
+                    {
+                        qry.Query = "<Where><Eq><FieldRef Name='FiscalYear' /><Value Type='Lookup'>" + year + "</Value></Eq></Where>";
+
+
+                    }
+                    else
+                    {
+                        qry.Query = "<Where><Neq><FieldRef Name='FiscalYear' /><Value Type='Lookup'>" + currenyear + "</Value></Neq></Where>";
+                    }
+
+                    SPListItemCollection itemCollection = list.GetItems(qry); ;
+                    DataTable dt = new DataTable();
+                    dt.Columns.Add("Title");
+                    dt.Columns.Add("Description");
+                    dt.Columns.Add("ImagePath");
+                    dt.Columns.Add("pageURL");
+
+
+                    if (itemCollection.Count > 0)
+                    {
+                        foreach (SPListItem item in itemCollection)
+                        {
+                            DataRow row = dt.NewRow();
+                            row["Title"] = item["Title"].ToString();
+
+                            StringBuilder st = new StringBuilder();
+                            StringBuilder sbmore = new StringBuilder();
+
+                            SPFieldMultiLineText multilineField = item.Fields.GetField("EventDescription") as SPFieldMultiLineText;
+                            string htmltext = multilineField.GetFieldValueAsHtml(item["EventDescription"], item);
+                            string normaltext = multilineField.GetFieldValueAsText(item["EventDescription"]);
+                            sbmore.Append(normaltext);
+
+                            string text = sbmore.ToString();
+
+                            renderHtml.InnerHtml = sbmore.ToString();
+
+                            renderHtml.InnerText = sbmore.ToString();
+
+                            text = renderHtml.InnerText;
+                            
+                            if (item["EventDescription"].ToString().Length > 400)
+                            {
+                                st.Append(item["EventDescription"].ToString().Substring(0, 400) +"...." + "<a id='trigger' onmouseover ='mouseoveritem(&quot; " + text + " &quot;)'>More</a>");
+
+                            }
+                            else
+                            {
+                                st.Append(item["EventDescription"].ToString());
+                            }
+
+                            row["Description"] = st.ToString();
+
+
+                            string attachmentInfo = String.Empty;
+                            if (item.Attachments.Count > 0)
+                            {
+                                foreach (string filename in item.Attachments)
+                                {
+                                    attachmentInfo += item.Attachments.UrlPrefix + filename;
+
+                                }
+                            }
+                            else {
+                                attachmentInfo = "/_layouts/15/VrtxIntranetStyles/Images/events.jpg";
+                            }
+                            if (item["PictureLibUrl"] != null)
+                            {
+
+                                SPFieldUrlValue typedValue = new SPFieldUrlValue(item["PictureLibUrl"].ToString());
+
+                                if (typedValue.Url != string.Empty)
+                                {
+                                    row["pageURL"] = typedValue.Url;
+                                }
+                               
+                            }
+                            else
+                            {
+                                //row["pageURL"] =string.Empty; 
+                            }
+
+                            row["ImagePath"] = attachmentInfo;
+                           
+                            dt.Rows.Add(row);
+                        }
+
+                        gridImage.DataSource = dt;
+                        gridImage.DataBind();
+
+                    }
+                    else
+                    {
+                        lblError.Visible = true;
+                        lblError.Text = "No Data Found";
+                    }
+                }
+
+            }
+        }
+    }
+}
+
