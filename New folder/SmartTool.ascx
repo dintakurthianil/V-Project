ba89c950b808e8ad2b81a7cda51dedcb8241ba0a
dintakurthianil <anilkumar.d@inhycgd6166.vertexcs.com>
2015-04-03 08:16:49 -0700

    SmartTools
    SmartTools

---
+++ New folder/SmartTool.ascx
@@ -0,0 +1,94 @@
+﻿<%@ Assembly Name="$SharePoint.Project.AssemblyFullName$" %>
+<%@ Assembly Name="Microsoft.Web.CommandUI, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+<%@ Register TagPrefix="SharePoint" Namespace="Microsoft.SharePoint.WebControls" Assembly="Microsoft.SharePoint, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+<%@ Register TagPrefix="Utilities" Namespace="Microsoft.SharePoint.Utilities" Assembly="Microsoft.SharePoint, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+<%@ Register TagPrefix="asp" Namespace="System.Web.UI" Assembly="System.Web.Extensions, Version=4.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35" %>
+<%@ Import Namespace="Microsoft.SharePoint" %>
+<%@ Register TagPrefix="WebPartPages" Namespace="Microsoft.SharePoint.WebPartPages" Assembly="Microsoft.SharePoint, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="SmartTool.ascx.cs" Inherits="VrtxIntranetPortal.SmartTool.SmartTool" %>
+
+
+
+<SharePoint:CssRegistration runat="server" Name="/_layouts/15/VrtxIntranetStyles/CSS/intranet.css" />
+
+<span style="font-weight: bold">Smart Tools - India</span>
+<div id="Container" >
+    <asp:GridView ID="gvIndiaSmartTool" runat="server" AutoGenerateColumns="false" Width="850px" BorderStyle="0" GridLines="None" CssClass="">
+        <AlternatingRowStyle CssClass="grid-sub2" BorderWidth="0" />
+        <RowStyle CssClass="grid-sub1" BorderWidth="0" />
+        <HeaderStyle CssClass="grid-header" />
+
+        <Columns>
+            <asp:TemplateField HeaderText="S.No" HeaderStyle-HorizontalAlign="Left">
+                <ItemTemplate>
+                    <asp:Label ID="lblIndiaSerialNo" Text='<%#Container.DataItemIndex + 1%>' Width="50px" runat="server"></asp:Label>
+                </ItemTemplate>
+            </asp:TemplateField>
+            <asp:TemplateField HeaderText="URL" HeaderStyle-HorizontalAlign="Left">
+                <ItemTemplate>
+                    <asp:HyperLink ID="HyperLink1" runat="server" Width="200px"
+                        NavigateUrl='<%# Eval("URL") %>'
+                        Text='<%# Eval("ToolName") %>' Target="_blank"></asp:HyperLink>
+
+                </ItemTemplate>
+            </asp:TemplateField>
+            <asp:TemplateField HeaderText="Tool Name" HeaderStyle-HorizontalAlign="Left">
+                <ItemTemplate>
+                    <asp:Label ID="lblIndiaToolName" Width="200px" Text='<%#Eval("ToolName")%>' runat="server"></asp:Label>
+                </ItemTemplate>
+            </asp:TemplateField>
+            <asp:TemplateField HeaderText="Country" HeaderStyle-HorizontalAlign="Left">
+                <ItemTemplate>
+                    <asp:Label ID="lblIndiaCountry" Width="200px" Text='<%#Eval("Country")%>' runat="server"></asp:Label>
+                </ItemTemplate>
+            </asp:TemplateField>
+            <asp:TemplateField HeaderText="Notes" HeaderStyle-HorizontalAlign="Left">
+                <ItemTemplate>
+                    <asp:Label ID="lblIndiaNotes" Width="200px" Text='<%#Eval("Notes")%>' runat="server"></asp:Label>
+                </ItemTemplate>
+            </asp:TemplateField>
+        </Columns>
+    </asp:GridView>
+</div>
+<br />
+<br />
+<br />
+<span style="font-weight: bold">Smart Tools - USA</span>
+<asp:GridView ID="gvUSASmartTool" runat="server" AutoGenerateColumns="false" Width="850px" BorderStyle="0" GridLines="None" CssClass="">
+    <AlternatingRowStyle CssClass="grid-sub2" BorderWidth="0" />
+    <RowStyle CssClass="grid-sub1" BorderWidth="0" />
+    <HeaderStyle CssClass="grid-header" />
+
+    <Columns>
+
+        <asp:TemplateField HeaderText="S.No" HeaderStyle-HorizontalAlign="Left">
+
+            <ItemTemplate>
+
+                <asp:Label ID="lblUSASerialNo" Text='<%#Container.DataItemIndex + 1%>' Width="50px" runat="server"></asp:Label>
+            </ItemTemplate>
+        </asp:TemplateField>
+        <asp:TemplateField HeaderText="URL" HeaderStyle-HorizontalAlign="Left">
+            <ItemTemplate>
+                <asp:HyperLink ID="HyperLink2" runat="server" Width="200px"
+                    NavigateUrl='<%# Eval("URL") %>'
+                    Text='<%# Eval("ToolName") %>' Target="_blank"></asp:HyperLink>
+            </ItemTemplate>
+        </asp:TemplateField>
+        <asp:TemplateField HeaderText="ToolName" HeaderStyle-HorizontalAlign="Left">
+            <ItemTemplate>
+                <asp:Label ID="lblUSAToolName" Width="200px" Text='<%#Eval("ToolName")%>' runat="server"></asp:Label>
+            </ItemTemplate>
+        </asp:TemplateField>
+        <asp:TemplateField HeaderText="Country" HeaderStyle-HorizontalAlign="Left">
+            <ItemTemplate>
+                <asp:Label ID="lblUSACountry" Width="200px" Text='<%#Eval("Country")%>' runat="server"></asp:Label>
+            </ItemTemplate>
+        </asp:TemplateField>
+        <asp:TemplateField HeaderText="Notes" HeaderStyle-HorizontalAlign="Left">
+            <ItemTemplate>
+                <asp:Label ID="lblUSANotes" Width="200px" Text='<%#Eval("Notes")%>' runat="server"></asp:Label>
+            </ItemTemplate>
+        </asp:TemplateField>
+    </Columns>
+</asp:GridView>
---
+++ New folder/SmartTool.ascx.cs
@@ -0,0 +1,141 @@
+﻿using Microsoft.SharePoint;
+using System;
+using System.ComponentModel;
+using System.Data;
+using System.Web.UI.WebControls.WebParts;
+
+namespace VrtxIntranetPortal.SmartTool
+{
+    [ToolboxItemAttribute(false)]
+    public partial class SmartTool : WebPart
+    {
+        // Uncomment the following SecurityPermission attribute only when doing Performance Profiling on a farm solution
+        // using the Instrumentation method, and then remove the SecurityPermission attribute when the code is ready
+        // for production. Because the SecurityPermission attribute bypasses the security check for callers of
+        // your constructor, it's not recommended for production purposes.
+        // [System.Security.Permissions.SecurityPermission(System.Security.Permissions.SecurityAction.Assert, UnmanagedCode = true)]
+        public SmartTool()
+        {
+        }
+
+        protected override void OnInit(EventArgs e)
+        {
+            base.OnInit(e);
+            InitializeControl();
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           // txtNmae.Text = "helo";
+            GetSmartTools();
+        }
+
+        private void GetSmartTools()
+        {
+            using (SPSite site = new SPSite(SPContext.Current.Web.Url))
+            {
+
+                using (SPWeb web = site.OpenWeb())
+                {
+                    SPList list = web.Lists.TryGetList("Smart Tools");
+                    if (list != null)
+                    {
+
+                        SPQuery query = new SPQuery();
+                        string stringQuery = string.Format(@"<Where><Eq><FieldRef Name='Country' /><Value Type='LookUp'>{0}</Value></Eq></Where>", "India");
+                        query.Query = stringQuery;
+                        SPListItemCollection queryResults = list.GetItems(query);
+                        DataTable dt = new DataTable();
+
+                        dt.Columns.Add("URL", typeof(string));
+                        dt.Columns.Add("ToolName", typeof(string));
+                        dt.Columns.Add("Country", typeof(string));
+                        dt.Columns.Add("Notes", typeof(string));
+
+                        if (queryResults != null && queryResults.Count > 0)
+                        {
+                            foreach (SPListItem item in queryResults)
+                            {
+
+                                DataRow dr = dt.NewRow();
+                               
+                                if (item["URL"] != null)
+                                {
+                                    SPFieldUrlValue urlFiled = new SPFieldUrlValue(item["URL"].ToString());
+
+                                    string urldes = urlFiled.Description;
+                                    string urlNmae = urlFiled.Url;
+                                   // string ursl = "<a href =" + urlNmae + " target='_blank'>" + item["Tool_x0020_Name"] + "</a>";
+                                    dr["URL"] = item["URL"] = urlNmae;
+                                }
+                                else {
+                                    dr["URL"] = string.Empty;
+                                }
+
+                                
+                                dr["ToolName"] = item["Tool_x0020_Name"] != null ? item["Tool_x0020_Name"].ToString() : string.Empty;
+
+                                dr["Country"] = new SPFieldLookupValue(item["Country"] as String).LookupValue;
+                                dr["Notes"] = item["Notes"] != null ? item["Notes"].ToString() : string.Empty;
+                                dt.Rows.Add(dr);
+                            }
+                        }
+                        else
+                        {
+
+                        }
+
+                        gvIndiaSmartTool.DataSource = dt;
+                        gvIndiaSmartTool.DataBind();
+
+
+                        SPQuery query1 = new SPQuery();
+                        string stringQuery1 = string.Format(@"<Where><Eq><FieldRef Name='Country' /><Value Type='LookUp'>{0}</Value></Eq></Where>", "USA");
+                        query1.Query = stringQuery1;
+                        SPListItemCollection queryResults1 = list.GetItems(query1);
+                        DataTable dt1 = new DataTable();
+
+                        dt1.Columns.Add("URL", typeof(string));
+                        dt1.Columns.Add("ToolName", typeof(string));
+                        dt1.Columns.Add("Country", typeof(string));
+                        dt1.Columns.Add("Notes", typeof(string));
+                        if (queryResults1 != null && queryResults1.Count > 0)
+                        {
+                            foreach (SPListItem item in queryResults1)
+                            {
+
+                                DataRow dr = dt1.NewRow();
+                                if (item["URL"] != null)
+                                {
+                                    SPFieldUrlValue urlFiled = new SPFieldUrlValue(item["URL"].ToString());
+
+                                    string urldes = urlFiled.Description;
+                                    string urlNmae = urlFiled.Url;
+                                   // string ursl = "<a href=" + urlNmae + " target='_blank'>" + item["Tool_x0020_Name"] + "</a>";
+                                    dr["URL"] = item["URL"] = urlNmae;
+                                }
+                                else
+                                {
+                                    dr["URL"] = string.Empty;
+                                }
+                               
+                                dr["ToolName"] = item["Tool_x0020_Name"] != null ? item["Tool_x0020_Name"].ToString() : string.Empty;
+                                dr["Country"] = new SPFieldLookupValue(item["Country"] as String).LookupValue;
+                                dr["Notes"] = item["Notes"] != null ? item["Notes"].ToString() : string.Empty;
+                                dt1.Rows.Add(dr);
+                            }
+                        }
+                        else
+                        {
+
+                        }
+
+                        gvUSASmartTool.DataSource = dt1;
+                        gvUSASmartTool.DataBind();
+                    }
+                }
+            }
+
+        }
+    }
+}
