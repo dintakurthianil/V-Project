c92061ba3aefa669b43f70257540e9dc7b2e02cc
dintakurthianil <anilkumar.d@inhycgd6166.vertexcs.com>
2015-04-03 08:09:28 -0700

    CorporateOverview
    CorporateOverview

---
+++ New folder/Corporate Overview.ascx
@@ -0,0 +1,47 @@
+﻿<%@ Assembly Name="$SharePoint.Project.AssemblyFullName$" %>
+<%@ Assembly Name="Microsoft.Web.CommandUI, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %> 
+<%@ Register Tagprefix="SharePoint" Namespace="Microsoft.SharePoint.WebControls" Assembly="Microsoft.SharePoint, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %> 
+<%@ Register Tagprefix="Utilities" Namespace="Microsoft.SharePoint.Utilities" Assembly="Microsoft.SharePoint, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+<%@ Register Tagprefix="asp" Namespace="System.Web.UI" Assembly="System.Web.Extensions, Version=4.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35" %>
+<%@ Import Namespace="Microsoft.SharePoint" %> 
+<%@ Register Tagprefix="WebPartPages" Namespace="Microsoft.SharePoint.WebPartPages" Assembly="Microsoft.SharePoint, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="Corporate Overview.ascx.cs" Inherits="Company.Corporate_Overview.Corporate_Overview" %>
+
+
+
+
+
+<asp:GridView ID="gvCorporateOverview" runat="server" AutoGenerateColumns="false" Width ="100%" BorderStyle="0"  GridLines="None">
+    <AlternatingRowStyle CssClass="grid-sub2" BorderWidth="0" />
+    <RowStyle CssClass="grid-sub1" BorderWidth="0"  />
+    <HeaderStyle CssClass="" />
+
+    <Columns>
+        
+        <asp:TemplateField HeaderText="" HeaderStyle-HorizontalAlign="Left">
+            <ItemTemplate>
+                <table style="padding:10px 10px 10px 10px;">
+
+                    <tr>
+                        <td style="    font-size: large;    padding:  0px 15px 0px;    color:#133d8c;">
+                       <asp:Label ID="lblTittle" Text='<%#Eval("Title")%>'  runat="server"></asp:Label>
+                            </td>
+                    </tr>
+                    <tr>
+                        <td style="   padding: 0px 15px 0px; line-height:20px">
+                        <asp:Label ID="lblDesciption" Text='<%#Eval("Description")%>'  runat="server"></asp:Label>
+                        <td>
+                       
+                    </tr>
+                    
+                </table>
+            </ItemTemplate>
+        </asp:TemplateField>
+       
+    </Columns>
+</asp:GridView>
+
+<div style="padding-left: 30%;padding-top: 10%;">
+    <h2><asp:Label Text="" ID="lblerrormessage" Visible="false" runat="server" /></h2>
+
+</div>
---
+++ New folder/Corporate Overview.ascx.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.ComponentModel;
+using System.Web.UI.WebControls.WebParts;
+using Microsoft.SharePoint;
+using System.Data;
+using System.Web;
+
+namespace Company.Corporate_Overview
+{
+    [ToolboxItemAttribute(false)]
+    public partial class Corporate_Overview : WebPart
+    {
+        // Uncomment the following SecurityPermission attribute only when doing Performance Profiling on a farm solution
+        // using the Instrumentation method, and then remove the SecurityPermission attribute when the code is ready
+        // for production. Because the SecurityPermission attribute bypasses the security check for callers of
+        // your constructor, it's not recommended for production purposes.
+        // [System.Security.Permissions.SecurityPermission(System.Security.Permissions.SecurityAction.Assert, UnmanagedCode = true)]
+        public Corporate_Overview()
+        {
+        }
+
+        protected override void OnInit(EventArgs e)
+        {
+            base.OnInit(e);
+            InitializeControl();
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                if (HttpContext.Current.Request.QueryString["year"] != null)
+                {
+                    string Year = HttpContext.Current.Request.QueryString["year"].ToString();
+                    GetCorporateOverview(Year);
+                }
+                else
+                {
+                    string currentYear = DateTime.Now.Year.ToString();
+                    int year = Convert.ToInt32(currentYear);
+                    string yearvalue = Convert.ToString(year - 1);
+                    string yeare = yearvalue;// DateTime.Now.Year.ToString();
+                    GetCorporateOverview(yeare);
+                }
+
+            }
+
+        }
+        private void GetCorporateOverview(string year)
+        {
+            using (SPSite osite = new SPSite(SPContext.Current.Web.Url))
+            {
+                using (SPWeb oweb = osite.OpenWeb())
+                {
+
+
+                    SPList olist = oweb.Lists.TryGetList("CorporateOverview");
+                    SPQuery querry = new SPQuery();
+                    string query = string.Empty;
+                    if (year != "allyears")
+                    {
+                        query = string.Format(@"<Where><Eq><FieldRef Name='FiscalYear'/><Value Type='Lookup'>{0}</Value></Eq></Where>", year);
+                    }
+                    else
+                    {
+                        string yearval = DateTime.Now.Year.ToString();
+                        query = string.Format(@"<Where><Neq><FieldRef Name='FiscalYear' /><Value Type='Lookup'>" + yearval + "</Value></Neq></Where>");
+                    }
+                    querry.Query = query;
+                    SPListItemCollection collListItems = olist.GetItems(querry);
+
+                    DataTable dt = new DataTable();
+
+                    dt.Columns.Add("Title", typeof(string));
+                    dt.Columns.Add("Description", typeof(string));
+                    if (collListItems.Count > 0)
+                    {
+                        foreach (SPListItem item in collListItems)
+                        {
+                            DataRow dr = dt.NewRow();
+
+                            dr["Title"] = item["Title"] != null ? item["Title"].ToString() : string.Empty;
+                            dr["Description"] = item["Description"] != null ? item["Description"].ToString() : string.Empty;
+                            dt.Rows.Add(dr);
+                        }
+                        gvCorporateOverview.DataSource = dt;
+                        gvCorporateOverview.DataBind();
+                    }
+
+                    else
+                    {
+                        lblerrormessage.Visible = true;
+                        lblerrormessage.Text = "No Data Found";
+                    }
+                }
+            }
+        }
+    }
+}
