71da82168b613580f17aae9bae57c8f20fa7d0b8
dintakurthianil <anilkumar.d@inhycgd6166.vertexcs.com>
2015-04-03 08:11:59 -0700

    HomeContent
    HomeContent

---
+++ New folder/HomeContent.ascx
@@ -0,0 +1,256 @@
+﻿<%@ Assembly Name="$SharePoint.Project.AssemblyFullName$" %>
+<%@ Assembly Name="Microsoft.Web.CommandUI, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+<%@ Register TagPrefix="SharePoint" Namespace="Microsoft.SharePoint.WebControls" Assembly="Microsoft.SharePoint, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+<%@ Register TagPrefix="Utilities" Namespace="Microsoft.SharePoint.Utilities" Assembly="Microsoft.SharePoint, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+<%@ Register TagPrefix="asp" Namespace="System.Web.UI" Assembly="System.Web.Extensions, Version=4.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35" %>
+<%@ Import Namespace="Microsoft.SharePoint" %>
+<%@ Register TagPrefix="WebPartPages" Namespace="Microsoft.SharePoint.WebPartPages" Assembly="Microsoft.SharePoint, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="HomeContent.ascx.cs" Inherits="VrtxIntranetPortal.HomeContent.HomeContent" %>
+
+<script type="text/javascript" src="/_layouts/15/init.js"></script>
+<script type="text/javascript" src="/_layouts/15/sp.runtime.js"></script>
+<script type="text/javascript" src="/_layouts/15/sp.js"></script>
+<link href ="/_layouts/15/VrtxIntranetStyles/CSS/craftyslide.css" rel="stylesheet" />
+<style>
+
+ .button-india{
+	font-size:11px;
+	font-weight:bold;
+	line-height:16px;
+	color:#000;
+	text-align:center;
+	text-transform:uppercase;
+	width:47px;
+	margin: 15px 0;
+	float:left;
+}
+
+.button-india a{
+	background:#f16530;
+	color:#fff;
+	text-decoration:none;
+	margin:0;
+	padding:8px;
+
+}
+
+.button-india a:hover{
+	background:#133d8c;
+	color:#FFF;
+	text-decoration:none;
+	margin:0;
+	padding:8px;
+}	
+
+.button-usa{
+	font-size:11px;
+	font-weight:bold;
+	line-height:16px;
+	color:#000;
+	text-align:center;
+	text-transform:uppercase;
+	width:38px;
+	margin: 15px 0;
+	position:relative;
+	float:left;
+}
+
+.button-usa a{
+	background:#f16530;
+	color:#fff;
+	text-decoration:none;
+	margin:0;
+	padding:8px;
+
+}
+
+.button-usa a:hover{
+	background:#133d8c;
+	color:#FFF;
+	text-decoration:none;
+	margin:0;
+	padding:8px;
+}	
+
+
+.button-ind-active{
+	font-size:11px;
+	font-weight:bold;
+	line-height:16px;
+	color:#000;
+	text-align:center;
+	text-transform:uppercase;
+	width:47px;
+	margin: 15px 0;
+	position:relative;
+	float:left;
+}
+
+.button-ind-active a{
+	background:#133d8c;
+	color:#fff;
+	text-decoration:none;
+	margin:0;
+	padding:8px;
+
+}
+
+.button-usa-active{
+	font-size:11px;
+	font-weight:bold;
+	line-height:16px;
+	color:#000;
+	text-align:center;
+	text-transform:uppercase;
+	width:38px;
+	margin: 15px 0;
+	position:relative;
+	float:left;
+}
+
+.button-usa-active a{
+	background:#133d8c;
+	color:#fff;
+	text-decoration:none;
+	margin:0;
+	padding:8px;
+
+}
+
+.button-general{
+	font-size:11px;
+	font-weight:bold;
+	line-height:16px;
+	color:#000;
+	text-align:left;
+	text-transform:uppercase;
+	width:150px;
+	margin: 15px 0;
+	position:relative;
+	float:left;
+}
+
+</style>
+
+<div class="second-ctr">
+   
+    <div class="message-area">
+          <div class="message-ceo-ctr">
+           <div id="slideshow">
+        <ul>
+          <li>
+           <%-- <h2>Message From CEO</h2>--%>
+             <img runat="server" id="picture1"     width="100" height="100"  />
+            <h3>Organizational Announcement</h3>
+           
+              <p id="content1" runat="server"  style ="height:270px">
+              
+              </p>
+          
+            <div class="button-Readmore"><a href="/SitePages/MoreCEO.aspx" runat="server" target="_blank" >Read more1</a></div>
+          </li>
+          
+          <li>
+           <%-- <h2>Message From President</h2>--%>
+             <img  id="picture2" runat="server" alt="" width="100" height="100"  />
+            <h3>Organizational Announcement</h3>
+            <p id="content2" runat ="server" style ="height:270px"></p>
+            <div class="button-Readmore"><a href="/SitePages/MoreCEO.aspx" runat="server" target="_blank" >Read more2</a></div>
+          </li>
+          
+           <li>
+            <%--<h2>Message From Director</h2>--%>
+             <img id="picture3" runat="server"   alt="" width="100" height="100"  />
+            <h3>Organizational Announcement</h3>
+            <p id="content3" runat="server" style ="height:270px" ></p>
+            <div class="button-Readmore"><a  href="/SitePages/MoreCEO.aspx" runat="server" target="_blank" >Read more3</a></div>
+          </li>
+          
+                          
+        </ul> 
+   </div>
+          </div>
+        </div>
+    <div class="middle-area">
+
+        <div class="holidays">
+            <h2>Holidays of the month</h2>
+            <div id="Holidays" runat="server" style="height: 150px">
+            </div>
+            <div  class="button-india" id="divIndiaHoliday">
+                <a href="javascript:void(0)" id="btnHolidayIndia">India</a>
+            </div>
+            <div  class="button-usa"  id="divUSAHoliday">
+                <a href="javascript:void(0)" id="btnHolidayUSA">USA</a>
+            </div>
+            <div class="button-general">
+                <a href="#" id="hncMorHoliday" target ="_blank">More Holidays</a>
+            </div>
+
+        </div>
+        <div class="smalltools">
+            <h2>Smart tools</h2>
+            <div runat="server" id="divSmart" style="height: 120px"></div>
+            <div class="button-india"  id="idSmartIndia"><a href="javascript:void(0)" id="btnSmatToolIndia">INDIA</a></div>
+
+            <div class="button-usa" id="idSmartUsa"><a href="javascript:void(0)" id="btnSmatTooUSA">USA</a></div>
+            <div class="button-general"><a href="/SitePages/SmartTools.aspx">More Tools</a></div>
+        </div>
+    </div>
+    <div class="middle-area">
+        <div class="home-jobs" style="height:222px">
+               <h2>Job Openings @ Vertex</h2>
+            <a href="javascript:void(0)" >
+                <img src="/_layouts/15/VrtxIntranetStyles/Images/JobOpenings-img.jpg" height="150" width="245" style="border: 0" /></a>
+              <div class="button-india" id="divIndiaJob"><a id="ancIndia" href="http://careers.vertexcs.com/" target="_blank">INDIA</a></div>
+              <div class="button-usa" id="divUsaJob"><a id="ancUsa" href="http://www.vertexcs.com/current-openings-us" target="_blank">USA</a></div>
+            
+
+        </div>
+        <!---------Vertical Blocks (wats-new) ------->
+        <div class="home-news">
+            <h2>What's New @ Vertex</h2>
+            <div id="whatsNew" runat="server"></div>
+            <div id="divwhatsIndia" class="button-india"><a href="javascript:void(0)" id="btnWhatsNewIndia">INDIA</a></div>
+            <div id="divwhatsUsa"  class="button-usa"><a href="javascript:void(0)" id="btnWhatsNewUSA">USA</a></div>
+            <div class="button-general"><a href="/SitePages/MoreEvents.aspx">More Events</a></div>
+        </div>
+    </div>
+    <div class="profile-area">
+        <div class="profile-area-ctr">
+            <h2>Profile of the day</h2>
+            <asp:Image ID="imgProfile" runat="server" />
+            <h3 id="ProfileHeading" runat ="server" ></h3>
+            <p id="ProfileTxt" runat="server" ></p>
+            <div class="button-Readmore" id="btnProfile" runat="server" ><a href="#">Read more</a></div>
+        </div>
+    </div>
+
+
+
+</div>
+<asp:HiddenField runat ="server" ID="hdnCountry" />
+
+
+<script src="/_layouts/15/VrtxIntranetStyles/JS/craftyslide.min.js"></script>
+<script src="/_layouts/15/VrtxIntranetPortal/IntranetJS/HomeContent.js"></script>
+<script>
+
+    $("#slideshow").craftyslide();
+
+
+    function loadids(cntroltye) {
+        var varaid = "";
+        if (cntroltye == "SmatTool")
+            varaid = ('<%=divSmart.ClientID%>');
+        else if (cntroltye == "Holiday")
+            varaid = ('<%=Holidays.ClientID%>');
+        else if (cntroltye == "WhatsNews")
+            varaid = ('<%=whatsNew.ClientID%>');
+
+        else if (cntroltye == "hdnCountry")
+            varaid = ('<%=hdnCountry.ClientID%>');
+
+    return varaid;
+}
+</script>
---
+++ New folder/HomeContent.js
@@ -0,0 +1,409 @@
+﻿var clientContext = SP.ClientContext.get_current(); //gets the current context
+var web = clientContext.get_web(); //gets the web object
+var list; //gets the collection of lists
+var targetList;
+var results;
+var itemColl;
+var CEOMesageCollection;
+var divsmart = "divSmart";
+var itemIndex=0;
+var istrue = false;
+var isholidday = false;
+$(document).ready(function () {
+
+   
+    // ExecuteOrDelayUntilScriptLoaded(function () { getSmatTools() }, "SP.js");
+    $("#btnSmatToolIndia").click(function () {
+        var country = "India"
+        results = "";
+        itemColl = "";
+        list = "";
+        // ExecuteOrDelayUntilScriptLoaded(function () { getSmatTools(country) }, "SP.js");
+        $("#idSmartIndia").removeClass("button-india");
+        $("#idSmartIndia").addClass("button-ind-active");
+
+        $("#idSmartUsa").removeClass("button-usa-active");
+        $("#idSmartUsa").addClass("button-usa")
+        getSmatTools(country);
+    });
+
+    $("#btnSmatTooUSA").click(function () {
+        var country = "USA";
+        results = "";
+        itemColl = "";
+        list = "";
+        $("#idSmartUsa").removeClass("button-usa");
+        $("#idSmartUsa").addClass("button-usa-active");
+        $("#idSmartIndia").removeClass("button-ind-active");
+        $("#idSmartIndia").addClass("button-india");
+
+        
+        getSmatTools(country);
+        
+    });
+
+    $("#btnHolidayIndia").click(function () {
+        var country = "India"
+        results = "";
+        itemColl = "";
+        list = "";       
+        getHoliday(country);
+        isholidday = true;
+        
+        $("#hncMorHoliday").attr('href', "/SitePages/CurrentHolidays.aspx?ctype=india");
+        $("#divIndiaHoliday").removeClass("button-india");
+        $("#divIndiaHoliday").addClass("button-ind-active");
+
+        $("#divUSAHoliday").removeClass("button-usa-active");          
+        $("#divUSAHoliday").addClass("button-usa")
+
+    });
+
+    $("#btnHolidayUSA").click(function () {
+        var country = "USA";
+        results = "";
+        itemColl = "";
+        list = "";
+        isholidday = true;
+        //ExecuteOrDelayUntilScriptLoaded(function () { getSmatTools(country) }, "SP.js");
+        $("#divUSAHoliday").removeClass("button-usa");
+        $("#divUSAHoliday").addClass("button-usa-active");
+        $("#divIndiaHoliday").removeClass("button-ind-active");
+        $("#divIndiaHoliday").addClass("button-india");
+        getHoliday(country);
+        $("#hncMorHoliday").attr('href', "/SitePages/CurrentHolidays.aspx?ctype=usa");
+    });
+
+    
+    var hdnCountryId = loadids("hdnCountry");
+    var countryName = $("#" + hdnCountryId).val();
+    if (countryName == "India") {
+        $("#hncMorHoliday").attr('href', "/SitePages/CurrentHolidays.aspx?ctype=india");
+        $("#divIndiaHoliday").removeClass("button-india");
+        $("#divIndiaHoliday").addClass("button-ind-active");
+        $("#divUSAHoliday").addClass("button-usa");
+
+        $("#idSmartIndia").removeClass("button-india");
+        $("#idSmartIndia").addClass("button-ind-active");
+        $("#idSmartUsa").addClass("button-usa");
+
+        $("#divwhatsIndia").removeClass("button-india");
+        $("#divwhatsIndia").addClass("button-ind-active");
+        $("#divwhatsUsa").addClass("button-usa");
+
+        $("#divIndiaJob").removeClass("button-india");
+        $("#divIndiaJob").addClass("button-ind-active");
+        $("#divUsaJob").addClass("button-usa");
+
+           }
+
+    else {
+        $("#hncMorHoliday").attr('href', "/SitePages/CurrentHolidays.aspx?ctype=USA");
+        $("#divUSAHoliday").removeClass("button-usa");
+        $("#divUSAHoliday").addClass("button-usa-active");
+        $("#divIndiaHoliday").addClass("button-india");
+
+        $("#idSmartUsa").removeClass("button-usa");
+        $("#idSmartUsa").addClass("button-usa-active");
+        $("#idSmartIndia").addClass("button-india");
+
+        $("#divwhatsUsa").removeClass("button-usa");
+        $("#divwhatsUsa").addClass("button-usa-active");
+        $("#divwhatsIndia").addClass("button-india");
+
+        $("#divUsaJob").removeClass("button-usa");
+        $("#divUsaJob").addClass("button-usa-active");
+        $("#divIndiaJob").addClass("button-india");
+    }
+    
+
+    $("#btnWhatsNewIndia").click(function () {
+        var country = "India"
+        results = "";
+        itemColl = "";
+            list = "";
+        // ExecuteOrDelayUntilScriptLoaded(function () { getSmatTools(country) }, "SP.js");
+            $("#divwhatsIndia").removeClass("button-india");
+            $("#divwhatsIndia").addClass("button-ind-active");
+
+            $("#divwhatsUsa").removeClass("button-usa-active");
+            $("#divwhatsUsa").addClass("button-usa")
+            getWhatsNew(country);
+    });
+
+    $("#btnWhatsNewUSA").click(function () {
+        var country = "USA";
+        results = "";
+        itemColl = "";
+        list = "";
+        $("#divwhatsUsa").removeClass("button-usa");
+        $("#divwhatsUsa").addClass("button-usa-active");
+        $("#divwhatsIndia").removeClass("button-ind-active");
+        $("#divwhatsIndia").addClass("button-india");
+        //ExecuteOrDelayUntilScriptLoaded(function () { getSmatTools(country) }, "SP.js");
+        getWhatsNew(country);
+    });
+
+    $("#ancIndia").click(function () {
+        $("#divIndiaJob").removeClass("button-india");
+        $("#divIndiaJob").addClass("button-ind-active");
+
+        $("#divUsaJob").removeClass("button-usa-active");
+        $("#divUsaJob").addClass("button-usa")
+    });
+
+    $("#ancUsa").click(function () {
+        $("#divUsaJob").removeClass("button-usa");
+        $("#divUsaJob").addClass("button-usa-active");
+        $("#divIndiaJob").removeClass("button-ind-active");
+        $("#divIndiaJob").addClass("button-india");
+    });
+   
+    //hrNext
+
+    $("#hrNext").click(function () {
+       // getCEOItembyindexOne();
+    });
+   // getCEOMessage();
+});
+
+//function for get Smart Tools
+function getSmatTools(country) {
+    //debugger;
+    list = web.get_lists().getByTitle("Smart Tools");
+    var query1 = new SP.CamlQuery();
+    var stryquery = "<View><Query><Where><Eq><FieldRef Name='Country' /><Value Type='LookUp' >" + country + "</Value></Eq></Where></Query><RowLimit>4</RowLimit></View>";
+
+    query1.set_viewXml(stryquery);
+
+    itemColl = list.getItems(query1);
+
+    clientContext.load(itemColl);
+    clientContext.executeQueryAsync(retrieveListItemsSuccess, retrieveListItemsFail);
+}
+function retrieveListItemsSuccess() {
+    var iddiv = loadids("SmatTool");
+    $("#" + iddiv).html("");
+    if (itemColl.get_count() > 0) {
+
+        var listItemEnumerator = itemColl.getEnumerator();
+        result = "<ul>";
+        while (listItemEnumerator.moveNext()) {
+            var oListItem = listItemEnumerator.get_current();
+            var listDetails = "<li><a href=" + oListItem.get_item('URL').get_url() + " target='_blank'>" + oListItem.get_item('Tool_x0020_Name') + "</a></li>";
+            result = result + listDetails;
+        }
+        result = result + "</ul>";
+
+        $("#" + iddiv).html(result);
+    }
+    else {
+
+        $("#" + iddiv).html("No Smart Tools available");
+    }
+
+}
+// This function is executed if the above call fails
+function retrieveListItemsFail(sender, args) {
+    alert('Failed to get list items. Error:' + args.get_message());
+}
+//end function Smart Tool 
+
+function getHoliday(country) {
+   
+    list = web.get_lists().getByTitle("Holidays");
+    var query = new SP.CamlQuery();
+
+
+    var today = new Date();
+    var mm = today.getMonth()
+    var yyyy = today.getFullYear();
+    var monthStart = new Date(yyyy, mm, 1);
+    var monthEnd = new Date(yyyy, mm + 1, 1);
+    var firstday = monthStart.getFullYear() + "-" + (monthStart.getMonth() + 1) + "-" + monthStart.getDate();
+    var lastday = monthEnd.getFullYear() + "-" + (monthEnd.getMonth() + 1) + "-" + monthEnd.getDate();
+    
+  //var stryquery = "<View><Query><Where><And><And><And><Geq><FieldRef Name='HolidayDate' /><Value Type='DateTime'>" + firstday + "</Value></Geq><Leq><FieldRef Name='HolidayDate' /><Value Type='DateTime'>" + lastday + "</Value></Leq></And><Eq><FieldRef Name='Country' /><Value Type='Choice'>" + country + "</Value></Eq></And><Eq><FieldRef Name='FiscalYear' /><Value Type='Lookup'>" + yyyy + "</Value></Eq></And></Where></Query><RowLimit>4</RowLimit></View>";
+
+    var stryquery = "<View><Query><Where><And><And><And><Geq><FieldRef Name='HolidayDate' /><Value Type='DateTime'>" + firstday + "</Value></Geq><Leq><FieldRef Name='HolidayDate' /><Value Type='DateTime'>" + lastday + "</Value></Leq></And><Eq><FieldRef Name='Country' /><Value Type='Choice'>" + country + "</Value></Eq></And><Eq><FieldRef Name='FiscalYear' /><Value Type='Lookup'>" + yyyy + "</Value></Eq></And></Where><OrderBy><FieldRef Name='HolidayDate' Ascending='True' /></OrderBy></Query><RowLimit>3</RowLimit></View>";
+
+    query.set_viewXml(stryquery);
+
+    itemColl = list.getItems(query);
+
+    clientContext.load(itemColl);
+    clientContext.executeQueryAsync(retrieveHolidaysListItemsSuccess, retrieveHolidaysListItemsFail);
+}
+function retrieveHolidaysListItemsSuccess() {
+   
+    var iddiv = loadids("Holiday");
+    $("#" + iddiv).html("");
+    if (itemColl.get_count() > 0) {
+
+        var listItemEnumerator = itemColl.getEnumerator();
+        result = "<ul>";
+        var listDetails=""
+        while (listItemEnumerator.moveNext()) {
+            var oListItem = listItemEnumerator.get_current();
+             listDetails += "<li>";
+            if (oListItem.get_item('Country') == "India") {
+                listDetails += "<img src='/_layouts/15/VrtxIntranetStyles/Images/indian-flag.jpg' width='30' height='19' />";
+            }
+            else {
+                listDetails += "<img src='/_layouts/15/VrtxIntranetStyles/Images/usa-flag.jpg' width='30' height='19' />";
+            }
+
+            var hHadte = oListItem.get_item('HolidayDate')
+            var formatedDate = getDateFormat(hHadte)
+            listDetails += "<p>"+formatedDate +" "+oListItem.get_item('Title')+"</p>";
+            listDetails += "</li>";
+        
+        }
+        result = result + listDetails;
+        result = result + "</ul>";
+
+        $("#" + iddiv).html(result);
+    }
+    else {
+
+        $("#" + iddiv).html("<img src='/_layouts/15/VrtxIntranetStyles/Images/no-holidays.jpg' />");
+    }
+
+}
+function retrieveHolidaysListItemsFail(sender, args) {
+    alert('Failed to get list items. Error:' + args.get_message());
+}
+
+
+
+var countryval;
+
+function getWhatsNew(country) {
+    //debugger;
+    var today = new Date();
+    var mm = today.getMonth()
+    var date = today.getFullYear();
+    countryval = country;
+    list = web.get_lists().getByTitle("WhatsNew");
+    var query1 = new SP.CamlQuery();
+  //var stryquery = "<View><Query><Where><And><Eq><FieldRef Name='Country' /><Value Type='Choice'>{0}</Value></Eq><And><Eq><FieldRef Name='FiscalYear' /><Value Type='Lookup'>" + date + "</Value></Eq><Eq><FieldRef Name='ShowOnHomePage' /><Value Type='bit'>{1}</Value></Eq></And></And></Where><OrderBy><FieldRef Name= 'Modified' Ascending='FALSE'/></OrderBy></Query><RowLimit>1</RowLimit></View>";
+    var stryquery = "<View><Query><Where><And> <And><Eq><FieldRef Name='Country' /><Value Type='Choice'>" + country + "</Value></Eq><Eq><FieldRef Name='ShowOnHomePage' /><Value Type='bit'>1</Value></Leq></And><Eq><FieldRef Name='FiscalYear' /><Value Type='Lookup'>" + date + "</Value></Eq></And></Where><OrderBy><FieldRef Name= 'Modified' Ascending='FALSE'/></OrderBy></Query><RowLimit>1</RowLimit></View>"
+    query1.set_viewXml(stryquery);
+
+    itemColl = list.getItems(query1);
+
+    clientContext.load(itemColl);
+    clientContext.executeQueryAsync(retrieveWhatsNewListItemsSuccess, retrievewhatsNewListItemsFail);
+}
+function retrieveWhatsNewListItemsSuccess() {
+   //debugger// ebugger;
+    var iddiv = loadids("WhatsNews");
+    $("#" + iddiv).html("");
+    if (itemColl.get_count() > 0) {
+
+        var listItemEnumerator = itemColl.getEnumerator();
+        result = "";
+        while (listItemEnumerator.moveNext()) {
+            var oListItem = listItemEnumerator.get_current();
+            var eventTitle = oListItem.get_item('Title');
+            var evnetDescription = oListItem.get_item('EventDescription');
+            var eventdate = oListItem.get_item('EventDate');
+            var listDetails = "<b>"+eventTitle + "</b><br/>";
+            listDetails += "<b>" + getDateFormat(eventdate) + "</b><br/>";
+            listDetails += "<p>"+evnetDescription.substring(0,200)+"...."+"</p>";
+            result = result + listDetails;
+        }
+       
+
+        $("#" + iddiv).html(result);
+    }
+    else {
+
+        $("#" + iddiv).html("No Events available ");
+    }
+
+}
+// This function is executed if the above call fails
+function retrievewhatsNewListItemsFail(sender, args) {
+    alert('Failed to get list items. Error:' + args.get_message());
+}
+
+//--------------------------------
+function getDateFormat(listdate)
+{
+    var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+    var day = days[listdate.getDay()];
+    var month = months[listdate.getMonth()];
+    var year=listdate.getFullYear();
+   // September 09, 2014 (Monday) Ganesh Chaturthi
+
+    return month + " " + listdate.getDate() + ", " + listdate.getFullYear() + " " + "(" + day+")";
+
+}
+//===============================more announcement==================================
+/*
+function getCEOMessage()
+{
+    //debugger;
+    list = web.get_lists().getByTitle("CEOMessage");
+    var queryCEO = new SP.CamlQuery();
+    var stryquery = "<View><RowLimit>3</RowLimit></View>"
+    queryCEO.set_viewXml(stryquery);
+
+    CEOMesageCollection = list.getItems(queryCEO);
+
+    clientContext.load(CEOMesageCollection);
+   clientContext.executeQueryAsync(retrieveCEOMessageListItemsSuccess, retrieveCEOMessageListItemsFail);
+
+}*/
+  /*
+function getCEOItembyindexOne()
+{
+  debugger;
+    var iddiv = loadids("divmessage");
+    $("#" + iddiv).html("");
+    var listCount = CEOMesageCollection.get_count();
+    if (listCount > 0) 
+       
+        if(2 == itemIndex) 
+        {
+            itemIndex = 0; 
+            
+        }
+        else
+         itemIndex = itemIndex + 1;
+    
+        var listItem = CEOMesageCollection.itemAt(itemIndex);
+        // var CEOTitle = listItem.get_item('Title');
+        var CEODesc = listItem.get_item('Message')
+       // var attachments = listItem.get_item('Attachments');
+        var attachments = listItem.get_attachmentFiles();
+        clientContext.load(attachments);
+
+        clientContext.executeQueryAsync(Function.createDelegate(this, function(sender, args){
+            debugger;
+            var enums = attachments.getEnumerator();
+            while (enums.moveNext()) { 
+                var attacha = enums.get_current();
+                var attachemurl=attacha.get_serverRelativeUrl();
+            } 
+            
+        }), Function.createDelegate(this, function(){})); Function.createDelegate(this, function(){});
+
+
+        $("#" + iddiv).html(CEODesc);
+        
+    
+    
+}
+function retrieveCEOMessageListItemsSuccess() {
+    //debugger;
+    return CEOMesageCollection;
+
+}
+function retrieveCEOMessageListItemsFail(sender, args) {
+    alert('Failed to get list items. Error:' + args.get_message());
+}*/
+//======================================end more announcement========
